b81300bf31ca0dc46553429331cf0ad160e8b64f
Wiktor Kopec <wiktork@microsoft.com>
2021-04-02 15:48:51 -0700

    Change default values for top level properties. - Also relax schema restrictions

--- src/Tests/Microsoft.Diagnostics.Monitoring.ConfigurationSchema/SchemaGenerator.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.ConfigurationSchema/SchemaGenerator.cs
@@ -16,10 +16,23 @@ namespace Microsoft.Diagnostics.Monitoring.ConfigurationSchema
             schema.Id = @"https://www.github.com/dotnet/dotnet-monitor";
             schema.Title = "DotnetMonitorConfiguration";
 
+            //Allow other properties in the schema.
+            schema.AdditionalPropertiesSchema = JsonSchema.CreateAnySchema();
+
             //HACK Even though Properties is defined as JsonDataExtension, it still emits a property
             //called 'Properties' into the schema, instead of just specifying additionalProperties.
             //There is likely a more elegant way to fix this.
             schema.Definitions[nameof(EgressProvider)].Properties.Remove(nameof(EgressProvider.Properties));
+
+            //TODO Figure out a better way to add object defaults
+            schema.Definitions[nameof(EgressOptions)].Properties[nameof(EgressOptions.Providers)].Default = JsonSchema.CreateAnySchema();
+            schema.Definitions[nameof(EgressOptions)].Properties[nameof(EgressOptions.Properties)].Default = JsonSchema.CreateAnySchema();
+
+            //Make the default for each property and empty object.
+            foreach (KeyValuePair<string, JsonSchemaProperty> kvp in schema.Properties)
+            {
+                kvp.Value.Default = JsonSchema.CreateAnySchema();
+            }
 
             string schemaPayload = schema.ToJson();
 
