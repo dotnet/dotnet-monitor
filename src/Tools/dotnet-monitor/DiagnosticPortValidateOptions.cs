93a802d4ae1fccf59c067181b5debe05d7b8b807
Justin Anderson <jander-msft@users.noreply.github.com>
2021-04-12 12:39:08 -0700

    Validate that diagnostic port endpoint name is specified if running in listen mode. (#154)
    * Validate that diagnostic port endpoint name is specified if running in listen mode.
    
    * Send options validation failures through logging.

---
+++ src/Tools/dotnet-monitor/DiagnosticPortValidateOptions.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Diagnostics.Tools.Monitor
+{
+    internal class DiagnosticPortValidateOptions :
+        IValidateOptions<DiagnosticPortOptions>
+    {
+        public ValidateOptionsResult Validate(string name, DiagnosticPortOptions options)
+        {
+            if (options.ConnectionMode == DiagnosticPortConnectionMode.Listen
+                && string.IsNullOrEmpty(options.EndpointName))
+            {
+                return ValidateOptionsResult.Fail("In 'Listen' mode, the diagnostic port endpoint name must be specified.");
+            }
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
