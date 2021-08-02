3ba91c1f1e9f87bb838146cb1d513144b8a62e7d
kkeirstead <kkeirstead@microsoft.com>
2021-08-02 13:10:40 -0700

    Removed unnecessary usings

--- src/Tools/dotnet-monitor/FormatterNameOptions.cs
+++ src/Tools/dotnet-monitor/FormatterNameOptions.cs
@@ -3,12 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Diagnostics.Monitoring.WebApi;
-#if !UNITTEST
-using Microsoft.Diagnostics.Tools.Monitor.Egress.AzureBlob;
-using Microsoft.Diagnostics.Tools.Monitor.Egress.FileSystem;
-using Microsoft.Extensions.Logging;
-#endif
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
