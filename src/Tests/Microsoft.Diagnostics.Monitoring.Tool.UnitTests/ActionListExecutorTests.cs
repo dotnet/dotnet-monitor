a506530f713127725c6c8a23e45f31cde7c30bd8
kkeirstead <kkeirstead@microsoft.com>
2021-08-20 12:36:04 -0700

    Checking in before pulling in latest changes from feature/triggers

---
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/ActionListExecutorTests.cs
@@ -0,0 +1,206 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options.Actions;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Actions;
+using System.Reflection;
+using Microsoft.Diagnostics.Monitoring.TestCommon;
+using System.Threading;
+using System;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Diagnostics.Tools.Monitor;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
+{
+    public sealed class ActionListExecutorTests
+    {
+        [Fact]
+        public async Task ActionListExecutor_MultipleExecute()
+        {
+            ActionListExecutor executor = new();
+
+
+
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            options.Path = DotNetHost.HostExePath;
+            options.Arguments = GenerateArgumentsString(new string[] { "ZeroExitCode" });
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+            CollectionRuleActionResult result = await action.ExecuteAsync(options, null, cancellationTokenSource.Token);
+
+            ValidateActionResult(result, "0");
+        }
+
+        [Fact]
+        public async Task ExecuteAction_NonzeroExitCode()
+        {
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            options.Path = DotNetHost.HostExePath;
+            options.Arguments = GenerateArgumentsString(new string[] { "NonzeroExitCode" });
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+            InvalidOperationException invalidOperationException = await Assert.ThrowsAsync<InvalidOperationException>(
+                () => action.ExecuteAsync(options, null, cancellationTokenSource.Token));
+
+            Assert.Contains(string.Format(Strings.ErrorMessage_NonzeroExitCode, "1"), invalidOperationException.Message);
+        }
+
+        [Fact]
+        public async Task ExecuteAction_TokenCancellation()
+        {
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            options.Path = DotNetHost.HostExePath;
+            options.Arguments = GenerateArgumentsString(new string[] { "Sleep", (TokenTimeoutMs + DelayMs).ToString() }); ;
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+            TaskCanceledException taskCanceledException = await Assert.ThrowsAsync<TaskCanceledException>(
+                () => action.ExecuteAsync(options, null, cancellationTokenSource.Token));
+        }
+
+        [Fact]
+        public async Task ExecuteAction_TextFileOutput()
+        {
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            DirectoryInfo outputDirectory = null;
+
+            try
+            {
+                outputDirectory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "ExecuteAction", Guid.NewGuid().ToString()));
+                string textFileOutputPath = Path.Combine(outputDirectory.FullName, "file.txt");
+
+                const string testMessage = "TestMessage";
+
+                options.Path = DotNetHost.HostExePath;
+                options.Arguments = GenerateArgumentsString(new string[] { "TextFileOutput", textFileOutputPath, testMessage });
+
+                using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+                CollectionRuleActionResult result = await action.ExecuteAsync(options, null, cancellationTokenSource.Token);
+
+                ValidateActionResult(result, "0");
+
+                Assert.Equal(testMessage, File.ReadAllText(textFileOutputPath));
+            }
+            finally
+            {
+                try
+                {
+                    outputDirectory?.Delete(recursive: true);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ExecuteAction_InvalidPath()
+        {
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            string uniquePathName = Guid.NewGuid().ToString();
+
+            options.Path = uniquePathName;
+            options.Arguments = GenerateArgumentsString(Array.Empty<string>());
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+            FileNotFoundException fileNotFoundException = await Assert.ThrowsAsync<FileNotFoundException>(
+                () => action.ExecuteAsync(options, null, cancellationTokenSource.Token));
+
+            Assert.Equal(string.Format(Strings.ErrorMessage_FileNotFound, uniquePathName), fileNotFoundException.Message);
+        }
+
+        [Fact]
+        public async Task ExecuteAction_IgnoreExitCode()
+        {
+            ExecuteAction action = new();
+
+            ExecuteOptions options = new();
+
+            options.Path = DotNetHost.HostExePath;
+            options.Arguments = GenerateArgumentsString(new string[] { "NonzeroExitCode" });
+            options.IgnoreExitCode = true;
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TokenTimeoutMs);
+
+            CollectionRuleActionResult result = await action.ExecuteAsync(options, null, cancellationTokenSource.Token);
+
+            ValidateActionResult(result, "1");
+        }
+
+        private static string GenerateArgumentsString(string[] additionalArgs)
+        {
+            Assembly currAssembly = Assembly.GetExecutingAssembly();
+
+            List<string> args = new();
+
+            // Entrypoint assembly
+            args.Add(AssemblyHelper.GetAssemblyArtifactBinPath(currAssembly, "Microsoft.Diagnostics.Monitoring.ExecuteActionApp", TargetFrameworkMoniker.NetCoreApp31));
+
+            // Entrypoint arguments
+            args.AddRange(additionalArgs);
+
+            return string.Join(' ', args);
+        }
+
+        private static void ValidateActionResult(CollectionRuleActionResult result, string expectedExitCode)
+        {
+            string actualExitCode;
+
+            Assert.NotNull(result.OutputValues);
+            Assert.True(result.OutputValues.TryGetValue("ExitCode", out actualExitCode));
+            Assert.Equal(expectedExitCode, actualExitCode);
+        }
+
+        private async Task Validate()
+        {
+            IHost host = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.ConfigureCollectionRules();
+                    services.ConfigureEgress();
+                })
+                .Build();
+
+            try
+            {
+                validate(host.Services.GetRequiredService<IOptionsMonitor<CollectionRuleOptions>>());
+            }
+            finally
+            {
+                if (host is IAsyncDisposable asyncDisposable)
+                {
+                    await asyncDisposable.DisposeAsync();
+                }
+                else
+                {
+                    host.Dispose();
+                }
+            }
+        }
+    }
+}
--- src/Tools/dotnet-monitor/CollectionRules/ActionListExecutor.cs
+++ src/Tools/dotnet-monitor/CollectionRules/ActionListExecutor.cs
@@ -4,6 +4,7 @@
 
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Configuration;
 using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options;
 using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options.Actions;
 using System;
@@ -18,14 +19,84 @@ namespace Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Actions
 {
     internal sealed class ActionListExecutor
     {
-        public async Task<CollectionRuleActionResult> ExecuteActions(List<CollectionRuleActionOptions> collectionRuleActionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        private readonly ICollectionRuleActionOptionsProvider _actionOptionsProvider;
+
+        public ActionListExecutor(
+            ICollectionRuleActionOptionsProvider actionOptionsProvider)
         {
+            _actionOptionsProvider = actionOptionsProvider;
+        }
+
+        public async Task<List<CollectionRuleActionResult>> ExecuteActions(List<CollectionRuleActionOptions> collectionRuleActionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            List<CollectionRuleActionResult> actionResults = new List<CollectionRuleActionResult>();
+
             foreach (CollectionRuleActionOptions actionOption in collectionRuleActionOptions)
             {
-
-
-                Type.GetType(actionOption.Type) options = actionOption.Settings;
+                CollectionRuleActionResult result = await IdentifyCollectionRuleAction(actionOption, endpointInfo, cancellationToken);
+                actionResults.Add(result);
             }
+
+            return actionResults;
+        }
+
+        private async Task<CollectionRuleActionResult> IdentifyCollectionRuleAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            switch (actionOptions.Type)
+            {
+                case "Execute":
+                    return await PerformExecuteAction(actionOptions, endpointInfo, cancellationToken);
+                case "CollectDump":
+                    return await PerformCollectDumpAction(actionOptions, endpointInfo, cancellationToken);
+                case "CollectGCDump":
+                    return await PerformCollectGCDumpAction(actionOptions, endpointInfo, cancellationToken);
+                case "CollectLogs":
+                    return await PerformCollectLogsAction(actionOptions, endpointInfo, cancellationToken);
+                case "CollectTrace":
+                    return await PerformCollectTraceAction(actionOptions, endpointInfo, cancellationToken);
+                default:
+                    throw new ArgumentException($"Invalid action type {actionOptions.Type}.");
+            }
+        }
+
+        private async Task<CollectionRuleActionResult> PerformExecuteAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            ExecuteAction action = new();
+            ExecuteOptions options = (ExecuteOptions)(actionOptions.Settings);
+
+            return await action.ExecuteAsync(options, endpointInfo, cancellationToken);
+        }
+
+        private async Task<CollectionRuleActionResult> PerformCollectDumpAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            ExecuteAction action = new();
+            ExecuteOptions options = (ExecuteOptions)(actionOptions.Settings);
+
+            return await action.ExecuteAsync(options, endpointInfo, cancellationToken);
+        }
+
+        private async Task<CollectionRuleActionResult> PerformCollectGCDumpAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            ExecuteAction action = new();
+            ExecuteOptions options = (ExecuteOptions)(actionOptions.Settings);
+
+            return await action.ExecuteAsync(options, endpointInfo, cancellationToken);
+        }
+
+        private async Task<CollectionRuleActionResult> PerformCollectLogsAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            ExecuteAction action = new();
+            ExecuteOptions options = (ExecuteOptions)(actionOptions.Settings);
+
+            return await action.ExecuteAsync(options, endpointInfo, cancellationToken);
+        }
+
+        private async Task<CollectionRuleActionResult> PerformCollectTraceAction(CollectionRuleActionOptions actionOptions, IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+        {
+            ExecuteAction action = new();
+            ExecuteOptions options = (ExecuteOptions)(actionOptions.Settings);
+
+            return await action.ExecuteAsync(options, endpointInfo, cancellationToken);
         }
     }
 }
