19a95dec7e218747b54701e37aa9e24a0a54c4c1
Justin Anderson <jander-msft@users.noreply.github.com>
2021-09-21 08:56:55 -0700

    Add collection rules to config command (#886)

--- src/Tools/dotnet-monitor/ConfigurationJsonWriter.cs
+++ src/Tools/dotnet-monitor/ConfigurationJsonWriter.cs
@@ -59,6 +59,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
             }
 
             //No sensitive information
+            ProcessChildSection(configuration, ConfigurationKeys.CollectionRules, includeChildSections: true);
             ProcessChildSection(configuration, ConfigurationKeys.CorsConfiguration, includeChildSections: true);
             ProcessChildSection(configuration, ConfigurationKeys.DiagnosticPort, includeChildSections: true);
             ProcessChildSection(configuration, ConfigurationKeys.Metrics, includeChildSections: true);
