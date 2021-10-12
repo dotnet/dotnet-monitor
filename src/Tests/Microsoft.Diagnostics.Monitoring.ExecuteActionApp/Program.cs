c457f154801fa46fd5963a4baa5cc0cf4c9a6415
kkeirstead <kkeirstead@microsoft.com>
2021-10-12 16:27:58 -0700

    Added a using statement to simplify the ExecuteActionApp

--- src/Tests/Microsoft.Diagnostics.Monitoring.ExecuteActionApp/Program.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.ExecuteActionApp/Program.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Diagnostics.Monitoring.Tool.UnitTests;
 using System;
 using System.IO;
 using System.Linq;
@@ -20,22 +21,22 @@ namespace Microsoft.Diagnostics.Monitoring.ExecuteActionApp
 
             switch (testType)
             {
-                case Tool.UnitTests.ActionTestsConstants.ZeroExitCode:
+                case ActionTestsConstants.ZeroExitCode:
                     Assert.Equal(0, testArgs.Length);
                     return 0;
 
-                case Tool.UnitTests.ActionTestsConstants.NonzeroExitCode:
+                case ActionTestsConstants.NonzeroExitCode:
                     Assert.Equal(0, testArgs.Length);
                     return 1;
 
-                case Tool.UnitTests.ActionTestsConstants.Sleep:
+                case ActionTestsConstants.Sleep:
                     Assert.Equal(1, testArgs.Length);
                     string delayArg = testArgs[0];
                     int delay = int.Parse(delayArg);
                     Thread.Sleep(delay);
                     return 0;
 
-                case Tool.UnitTests.ActionTestsConstants.TextFileOutput:
+                case ActionTestsConstants.TextFileOutput:
                     Assert.Equal(2, testArgs.Length);
                     string pathArg = testArgs[0];
                     string contentsArg = testArgs[1];
