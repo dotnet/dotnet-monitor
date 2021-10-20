aa626890934440624f15b0be5ceefde25484039f
Justin Anderson <jander-msft@users.noreply.github.com>
2021-10-19 18:37:38 -0700

    Validate egress provider exists before queue/start egress operation. (#1041)

--- src/Microsoft.Diagnostics.Monitoring.WebApi/IEgressService.cs
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/IEgressService.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi
 {
     internal interface IEgressService
     {
-        bool CheckProvider(string providerName);
+        void ValidateProvider(string providerName);
 
         Task<EgressResult> EgressAsync(
             string providerName,
