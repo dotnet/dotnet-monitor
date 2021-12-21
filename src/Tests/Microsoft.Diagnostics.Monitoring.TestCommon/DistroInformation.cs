7cf4f0887e281fafa1270a721ecf38b3f0c822fd
Justin Anderson <jander@microsoft.com>
2021-12-21 14:44:11 -0800

    Add test exclusions for non-full dump types on Alpine Linux.

---
+++ src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/DistroInformation.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Diagnostics.Monitoring.TestCommon
+{
+    public static class DistroInformation
+    {
+        private const string LinuxReleaseFilePath = "/etc/os-release";
+        private const string AlpineLinuxIdLine = "ID=alpine";
+
+        public static bool IsAlpineLinux
+        {
+            get
+            {
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    return false;
+
+                if (!File.Exists(LinuxReleaseFilePath))
+                    return false;
+
+                string releaseInfo = null;
+                try
+                {
+                    releaseInfo = File.ReadAllText(LinuxReleaseFilePath);
+                }
+                catch
+                {
+                }
+
+                return !string.IsNullOrWhiteSpace(releaseInfo) &&
+                    releaseInfo.Contains(AlpineLinuxIdLine);
+            }
+        }
+    }
+}
