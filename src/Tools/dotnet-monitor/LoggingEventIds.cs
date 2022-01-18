54c1a7f14acaa7eafac3b4948cde12df0ea1d131
Justin Anderson <jander-msft@users.noreply.github.com>
2022-01-18 11:54:17 -0800

    Convert LoggingEventIds to enum (#1332)
    Incorporate suggestion from @jander-msft and dealing with switch statements
    
    Co-authored-by: Patrick Fenelon <kelltrick@users.noreply.github.com>

--- src/Tools/dotnet-monitor/LoggingEventIds.cs
+++ src/Tools/dotnet-monitor/LoggingEventIds.cs
@@ -2,63 +2,81 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Extensions.Logging;
+using System;
+
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
     // The existing EventIds must not be duplicated, reused, or repurposed.
     // New logging events must use the next available EventId.
-    internal static class LoggingEventIds
+    internal enum LoggingEventIds
     {
-        public const int EgressProviderAdded = 1;
-        public const int EgressProviderInvalidOptions = 2;
-        public const int EgressProviderInvalidType = 3;
-        public const int EgressProviderValidatingOptions = 4;
-        public const int EgressCopyActionStreamToEgressStream = 5;
-        public const int EgressProviderOptionsValidationFailure = 6;
-        public const int EgressProviderOptionValue = 7;
-        public const int EgressStreamOptionValue = 8;
-        public const int EgressProviderFileName = 9;
-        public const int EgressProvideUnableToFindPropertyKey = 10;
-        public const int EgressProviderInvokeStreamAction = 11;
-        public const int EgressProviderSavedStream = 12;
-        public const int NoAuthentication = 13;
-        public const int InsecureAutheticationConfiguration = 14;
-        public const int UnableToListenToAddress = 15;
-        public const int BoundDefaultAddress = 16;
-        public const int BoundMetricsAddress = 17;
-        public const int OptionsValidationFailure = 18;
-        public const int RunningElevated = 19;
-        public const int DisabledNegotiateWhileElevated = 20;
-        public const int ApiKeyValidationFailure = 21;
-        public const int ApiKeyAuthenticationOptionsChanged = 22;
-        public const int LogTempApiKey = 23;
-        public const int DuplicateEgressProviderIgnored = 24;
-        public const int ApiKeyAuthenticationOptionsValidated = 25;
-        public const int NotifyPrivateKey = 26;
-        public const int DuplicateCollectionRuleActionIgnored = 27;
-        public const int DuplicateCollectionRuleTriggerIgnored = 28;
-        public const int CollectionRuleStarted = 29;
-        public const int CollectionRuleFailed = 30;
-        public const int CollectionRuleCompleted = 31;
-        public const int CollectionRulesStarted = 32;
-        public const int CollectionRuleActionStarted = 33;
-        public const int CollectionRuleActionCompleted = 34;
-        public const int CollectionRuleTriggerStarted = 35;
-        public const int CollectionRuleTriggerCompleted = 36;
-        public const int CollectionRuleActionsThrottled = 37;
-        public const int CollectionRuleActionFailed = 38;
-        public const int CollectionRuleActionsCompleted = 39;
-        public const int CollectionRulesStarting = 40;
-        public const int DiagnosticRequestCancelled = 41;
-        public const int CollectionRuleUnmatchedFilters = 42;
-        public const int CollectionRuleConfigurationChanged = 43;
-        public const int CollectionRulesStopping = 44;
-        public const int CollectionRulesStopped = 45;
-        public const int CollectionRuleCancelled = 46;
-        public const int DiagnosticRequestFailed = 47;
-        public const int InvalidActionReferenceToken = 48;
-        public const int InvalidActionReference = 49;
-        public const int InvalidActionResultReference = 50;
-        public const int ActionSettingsTokenizationNotSupported = 51;
-        public const int EndpointTimeout = 52;
+        EgressProviderAdded = 1,
+        EgressProviderInvalidOptions = 2,
+        EgressProviderInvalidType = 3,
+        EgressProviderValidatingOptions = 4,
+        EgressCopyActionStreamToEgressStream = 5,
+        EgressProviderOptionsValidationFailure = 6,
+        EgressProviderOptionValue = 7,
+        EgressStreamOptionValue = 8,
+        EgressProviderFileName = 9,
+        EgressProvideUnableToFindPropertyKey = 10,
+        EgressProviderInvokeStreamAction = 11,
+        EgressProviderSavedStream = 12,
+        NoAuthentication = 13,
+        InsecureAutheticationConfiguration = 14,
+        UnableToListenToAddress = 15,
+        BoundDefaultAddress = 16,
+        BoundMetricsAddress = 17,
+        OptionsValidationFailure = 18,
+        RunningElevated = 19,
+        DisabledNegotiateWhileElevated = 20,
+        ApiKeyValidationFailure = 21,
+        ApiKeyAuthenticationOptionsChanged = 22,
+        LogTempApiKey = 23,
+        DuplicateEgressProviderIgnored = 24,
+        ApiKeyAuthenticationOptionsValidated = 25,
+        NotifyPrivateKey = 26,
+        DuplicateCollectionRuleActionIgnored = 27,
+        DuplicateCollectionRuleTriggerIgnored = 28,
+        CollectionRuleStarted = 29,
+        CollectionRuleFailed = 30,
+        CollectionRuleCompleted = 31,
+        CollectionRulesStarted = 32,
+        CollectionRuleActionStarted = 33,
+        CollectionRuleActionCompleted = 34,
+        CollectionRuleTriggerStarted = 35,
+        CollectionRuleTriggerCompleted = 36,
+        CollectionRuleActionsThrottled = 37,
+        CollectionRuleActionFailed = 38,
+        CollectionRuleActionsCompleted = 39,
+        CollectionRulesStarting = 40,
+        DiagnosticRequestCancelled = 41,
+        CollectionRuleUnmatchedFilters = 42,
+        CollectionRuleConfigurationChanged = 43,
+        CollectionRulesStopping = 44,
+        CollectionRulesStopped = 45,
+        CollectionRuleCancelled = 46,
+        DiagnosticRequestFailed = 47,
+        InvalidActionReferenceToken = 48,
+        InvalidActionReference = 49,
+        InvalidActionResultReference = 50,
+        ActionSettingsTokenizationNotSupported = 51,
+        EndpointTimeout = 52,
+    }
+
+    internal static class LoggingEventIdsExtensions
+    {
+        public static EventId EventId(this LoggingEventIds enumVal)
+        {
+            string name = Enum.GetName(typeof(LoggingEventIds), enumVal);
+            int id = enumVal.Id();
+            return new EventId(id, name);
+        }
+        public static int Id(this LoggingEventIds enumVal)
+        {
+            int id = (int)enumVal;
+            return id;
+        }
     }
 }
