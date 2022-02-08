0a9bf00ce478f07512e77321544a8a39f3dcd64b
Justin Anderson <jander@microsoft.com>
2022-02-08 14:50:52 -0800

    Releast tool updates - Change NugetLayoutWorker to only consider files under the packages directory - Add checksum file to ChecksumAssets in publish manifest - Add unprocessed files to BlobAssets in publish manifest - Add --dry-run option to run tool without publishing

--- eng/release/DiagnosticsReleaseTool/DiagnosticsRepoHelpers.cs
+++ eng/release/DiagnosticsReleaseTool/DiagnosticsRepoHelpers.cs
@@ -7,8 +7,7 @@ namespace DiagnosticsReleaseTool.Util
         public static readonly string[] ProductNames = new []{ "dotnet-monitor", "dotnet-dotnet-monitor" };
         public static readonly string[] RepositoryUrls = new [] { "https://github.com/dotnet/dotnet-monitor", "https://dev.azure.com/dnceng/internal/_git/dotnet-dotnet-monitor" };
         internal static bool IsDockerUtilityFile(FileInfo arg) =>
-            arg.FullName.EndsWith(".nupkg.sha512")
-            || arg.FullName.EndsWith(".nupkg.version")
+            arg.FullName.EndsWith(".nupkg.version")
             || arg.FullName.EndsWith(".nupkg.buildversion");
     }
 }
