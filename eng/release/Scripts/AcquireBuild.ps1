501958304e2b14cda17d8296150cbe70d04df65d
Juan Hoyos <juan.s.hoyos@outlook.com>
2022-02-07 19:05:46 -0800

    Update AcquireBuild.ps1 (#1466)

--- eng/release/Scripts/AcquireBuild.ps1
+++ eng/release/Scripts/AcquireBuild.ps1
@@ -59,6 +59,7 @@ try {
         --azdev-pat $AzdoToken `
         --bar-uri $MaestroApiEndPoint `
         --password $MaestroToken `
+        --separated `
         --verbose
 
     if ($LastExitCode -ne 0) {
