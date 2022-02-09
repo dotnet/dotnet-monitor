33839ce67c410b7ea1030c251644a3ac7bf394ff
Justin Anderson <jander-msft@users.noreply.github.com>
2022-02-09 09:46:08 -0800

    Release tool updates (#1477)
    - Change NugetLayoutWorker to only consider files under the packages directory
    - Add checksum file to ChecksumAssets in publish manifest
    - Add unprocessed files to BlobAssets in publish manifest
    - Add --dry-run option to run tool without publishing

---
+++ eng/release/DiagnosticsReleaseTool/Common/SkipPublisher.cs
@@ -0,0 +1,27 @@
+﻿using ReleaseTool.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReleaseTool.Core
+{
+    internal class SkipPublisher : IPublisher
+    {
+        private readonly HashSet<string> _relativeOutputPaths = new(StringComparer.OrdinalIgnoreCase);
+
+        public void Dispose()
+        {
+        }
+
+        public Task<string> PublishFileAsync(FileMapping fileData, CancellationToken ct)
+        {
+            if (!_relativeOutputPaths.Add(fileData.RelativeOutputPath))
+            {
+                throw new InvalidOperationException($"File {fileData.LocalSourcePath} was already published to {fileData.RelativeOutputPath}.");
+            }
+
+            return Task.FromResult(fileData.RelativeOutputPath);
+        }
+    }
+}
