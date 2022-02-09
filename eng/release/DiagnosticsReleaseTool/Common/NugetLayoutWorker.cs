9a18cc774a7b21abf22156fb3ff25a2d7392d0e6
Justin Anderson <jander@microsoft.com>
2022-02-09 08:49:48 -0800

    PR Feedback

--- eng/release/DiagnosticsReleaseTool/Common/NugetLayoutWorker.cs
+++ eng/release/DiagnosticsReleaseTool/Common/NugetLayoutWorker.cs
@@ -1,11 +1,12 @@
+using System;
 using System.IO;
 
 namespace ReleaseTool.Core
 {
     public sealed class NugetLayoutWorker : PassThroughLayoutWorker
     {
-        public NugetLayoutWorker(string stagingPath) : base(
-            shouldHandleFileFunc: static file => file.Extension == ".nupkg" && file.Directory.Name == "packages",
+        public NugetLayoutWorker(string stagingPath, Func<FileInfo, bool> additionalFileConstraint = null) : base(
+            shouldHandleFileFunc: file => file.Extension == ".nupkg" && (null == additionalFileConstraint || additionalFileConstraint(file)),
             getRelativePublishPathFromFileFunc: static file => Helpers.GetDefaultPathForFileCategory(file, FileClass.Nuget),
             getMetadataForFileFunc: static file => Helpers.GetDefaultFileMetadata(file, FileClass.Nuget),
             stagingPath
