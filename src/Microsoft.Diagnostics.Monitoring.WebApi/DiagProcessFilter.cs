2f41c983e9ea3b717412ca4e986ec01566fc6fc6
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2022-02-25 16:17:03 -0500

    Simplified Process Filter Schema (#1540)

--- src/Microsoft.Diagnostics.Monitoring.WebApi/DiagProcessFilter.cs
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/DiagProcessFilter.cs
@@ -74,6 +74,29 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi
 
         private static DiagProcessFilterEntry TransformDescriptor(ProcessFilterDescriptor processFilterDescriptor)
         {
+            if (!string.IsNullOrWhiteSpace(processFilterDescriptor.ProcessId))
+            {
+                return new DiagProcessFilterEntry { Criteria = DiagProcessFilterCriteria.ProcessId, MatchType = DiagProcessFilterMatchType.Exact, Value = processFilterDescriptor.ProcessId };
+            }
+            else if (!string.IsNullOrWhiteSpace(processFilterDescriptor.ProcessName))
+            {
+                return new DiagProcessFilterEntry
+                {
+                    Criteria = DiagProcessFilterCriteria.ProcessName,
+                    MatchType = (processFilterDescriptor.MatchType == ProcessFilterType.Exact) ? DiagProcessFilterMatchType.Exact : DiagProcessFilterMatchType.Contains,
+                    Value = processFilterDescriptor.ProcessName
+                };
+            }
+            else if (!string.IsNullOrWhiteSpace(processFilterDescriptor.CommandLine))
+            {
+                return new DiagProcessFilterEntry
+                {
+                    Criteria = DiagProcessFilterCriteria.CommandLine,
+                    MatchType = (processFilterDescriptor.MatchType == ProcessFilterType.Exact) ? DiagProcessFilterMatchType.Exact : DiagProcessFilterMatchType.Contains,
+                    Value = processFilterDescriptor.CommandLine
+                };
+            }
+
             switch (processFilterDescriptor.Key)
             {
                 case ProcessFilterKey.ProcessId:
