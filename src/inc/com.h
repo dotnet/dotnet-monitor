bb0b488b675971b04b656c08501cbbb058b7c553
Justin Anderson <jander@microsoft.com>
2022-02-28 14:22:39 -0800

    Add test profiler project Add common include files Add PAL stubs

---
+++ src/inc/com.h
@@ -0,0 +1,104 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma once
+
+#define BEGIN_COM_MAP(t) \
+    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void **ppvObject) override \
+    { \
+        if (ppvObject == nullptr) \
+        { \
+            return E_POINTER; \
+        }
+
+#define COM_INTERFACE_ENTRY(i) \
+        if (riid == IID_##i) \
+        { \
+            *ppvObject = (i*)this; \
+            this->AddRef(); \
+            return S_OK; \
+        }
+
+#define END_COM_MAP() \
+        *ppvObject = nullptr; \
+        return E_NOINTERFACE; \
+    }
+
+template <class T>
+class ComPtr
+{
+private:
+    T* m_p;
+
+public:
+    ComPtr() : m_p(nullptr)
+    {
+    }
+
+    ComPtr(T* p)
+    {
+        if (nullptr != p)
+        {
+            p->AddRef();
+        }
+        m_p = p;
+    }
+
+    ~ComPtr()
+    {
+        if (nullptr != m_p)
+        {
+            m_p->Release();
+            m_p = nullptr;
+        }
+    }
+
+    void Attach(T* p)
+    {
+        if (m_p)
+        {
+            m_p->Release();
+        }
+        m_p = p;
+    }
+
+    T* Detach()
+    {
+        T* p = m_p;
+        m_p = nullptr;
+        return p;
+    }
+
+    T* operator=(T* p) throw()
+    {
+        if (m_p != p)
+        {
+            ComPtr(p).Swap(*this);
+        }
+        return *this;
+    }
+
+    T* operator->()
+    {
+        return m_p;
+    }
+
+    T** operator&()
+    {
+        return &m_p;
+    }
+
+    operator T*()
+    {
+        return m_p;
+    }
+
+private:
+    void Swap(ComPtr<T>& other)
+    {
+        T* p = m_p;
+        m_p = other.m_p;
+        other.m_p = p;
+    }
+};
