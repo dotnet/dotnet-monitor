363ab2ba6ab766ebd843dbd9a1c521a88652f416
Justin Anderson <jander@microsoft.com>
2022-03-10 10:51:40 -0800

    Pack and sign from combined build artifacts

--- eng/cipacksignpublish.cmd
+++ eng/cipacksignpublish.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "_commonArgs=-ci -prepareMachine -verbosity minimal -configuration Release"
+set "_commonArgs=-restore -ci -prepareMachine -verbosity minimal -configuration Release"
 set "_logDir=%~dp0..\artifacts\log\Release\"
 
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0common\Build.ps1""" %_commonArgs% -pack -sign -publish -noBl /bl:'%_logDir%PackSignPublish.binlog' %*"
