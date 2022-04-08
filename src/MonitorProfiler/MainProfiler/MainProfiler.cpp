30a3beb01544065d3362e3eb23a2d4f20ada333e
Justin Anderson <jander-msft@users.noreply.github.com>
2022-04-08 13:09:38 -0700

    Rename test profiler to be product profiler (#1714)

--- src/MonitorProfiler/MainProfiler/MainProfiler.cpp
+++ src/MonitorProfiler/MainProfiler/MainProfiler.cpp
@@ -2,7 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#include "EnvProfiler.h"
+#include "MainProfiler.h"
+#include "macros.h"
 #include "tstring.h"
 #include "tostream.h"
 #include <iostream>
@@ -11,13 +12,13 @@
 #define HRESULTHEX(x) \
    "0x" << std::setw(8) << std::setfill('0') << std::hex << x
 
-GUID EnvProfiler::GetClsid()
+GUID MainProfiler::GetClsid()
 {
     // {6A494330-5848-4A23-9D87-0E57BBF6DE79}
     return { 0x6A494330, 0x5848, 0x4A23,{ 0x9D, 0x87, 0x0E, 0x57, 0xBB, 0xF6, 0xDE, 0x79 } };
 }
 
-STDMETHODIMP EnvProfiler::Initialize(IUnknown *pICorProfilerInfoUnk)
+STDMETHODIMP MainProfiler::Initialize(IUnknown *pICorProfilerInfoUnk)
 {
     HRESULT hr = S_OK;
 
@@ -30,13 +31,13 @@ STDMETHODIMP EnvProfiler::Initialize(IUnknown *pICorProfilerInfoUnk)
 
     ULONG cchLen = 0;
     if (FAILED(hr = m_pCorProfilerInfo->GetEnvironmentVariable(
-        _T("EnvProfiler_TestVariable"),
+        _T("MainProfiler_TestVariable"),
         0,
         &cchLen,
         nullptr
         )))
     {
-        std::cerr << "Unable to get EnvProfiler_TestVariable length: " << HRESULTHEX(hr) << std::endl;
+        std::cerr << "Unable to get MainProfiler_TestVariable length: " << HRESULTHEX(hr) << std::endl;
         return hr;
     }
 
@@ -47,19 +48,28 @@ STDMETHODIMP EnvProfiler::Initialize(IUnknown *pICorProfilerInfoUnk)
     std::cout << "Getting variable value." << std::endl;
 
     if (FAILED(hr = m_pCorProfilerInfo->GetEnvironmentVariable(
-        _T("EnvProfiler_TestVariable"),
+        _T("MainProfiler_TestVariable"),
         cchLen,
         &cchLen,
         pwszTestVariableValue.get()
         )))
     {
-        std::cerr << "Unable to get EnvProfiler_TestVariable content: " << HRESULTHEX(hr) << std::endl;
+        std::cerr << "Unable to get MainProfiler_TestVariable content: " << HRESULTHEX(hr) << std::endl;
         return hr;
     }
 
     tstring tstrTestVariableValue(pwszTestVariableValue.get());
 
     std::cout << "Value: " << tstrTestVariableValue << std::endl;
+
+    return S_OK;
+}
+
+STDMETHODIMP MainProfiler::LoadAsNotficationOnly(BOOL *pbNotificationOnly)
+{
+    ExpectedPtr(pbNotificationOnly);
+
+    *pbNotificationOnly = TRUE;
 
     return S_OK;
 }
