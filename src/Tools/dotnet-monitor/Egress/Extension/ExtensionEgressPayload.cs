fbd68a3780e4171737e595af6ebe41250f38fa4b
Patrick Fenelon <pafenelo@microsoft.com>
2022-04-27 18:16:49 -0700

    Make class for settings transfer abstract

--- src/Tools/dotnet-monitor/Egress/Extension/ExtensionEgressPayload.cs
+++ src/Tools/dotnet-monitor/Egress/Extension/ExtensionEgressPayload.cs
@@ -9,8 +9,8 @@ namespace Microsoft.Diagnostics.Tools.Monitor.Egress
     public class ExtensionEgressPayload
     {
         public EgressArtifactSettings Settings { get; set; }
-        public Dictionary<string, string> Properties { get; set; }
-        public Dictionary<string, string> Configuration { get; set; }
+        public IDictionary<string, string> Properties { get; set; }
+        public IDictionary<string, string> Configuration { get; set; }
         public string ProfileName { get; set; }
     }
 }
