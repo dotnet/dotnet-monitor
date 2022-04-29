75a4b8f1eff54be98d8fafdee85c2ae06f5be90b
Patrick Fenelon <pafenelo@microsoft.com>
2022-04-29 13:28:03 -0700

    Fix spelling and comments

--- src/Tools/dotnet-monitor/Egress/Configuration/IEgressProviderConfigurationProvider.cs
+++ src/Tools/dotnet-monitor/Egress/Configuration/IEgressProviderConfigurationProvider.cs
@@ -9,13 +9,13 @@ using System.Collections.Generic;
 namespace Microsoft.Diagnostics.Tools.Monitor.Egress.Configuration
 {
     /// <summary>
-    /// Provides access to all of the egress provider configuraiton blocks that use the given <see cref="OptionsType"/>.
-    /// These configuration blocks will be from "Egress:{ProviderType}" where "{ProviderType}" is one of the strings definded in <see cref="ProviderTypes"/>.
+    /// Provides access to all of the egress provider configuration blocks that use the given <see cref="OptionsType"/>.
+    /// These configuration blocks will be from "Egress:{ProviderType}" where "{ProviderType}" is one of the strings defined in <see cref="ProviderTypes"/>.
     /// </summary>
     internal interface IEgressProviderConfigurationProvider
     {
         /// <summary>
-        /// The name of the categories defined in configuration of this options type. These keys can be passed to <see cref="GetConfigurationSection(string)"/> to get the specific configuration section.
+        /// The name of the provider types defined in configuration that use this options type. These keys can be passed to <see cref="GetConfigurationSection(string)"/> to get the specific configuration section.
         /// </summary>
         IEnumerable<string> ProviderTypes { get; }
 
@@ -27,19 +27,19 @@ namespace Microsoft.Diagnostics.Tools.Monitor.Egress.Configuration
         /// <summary>
         /// Gets the <see cref="IConfigurationSection"/> associated with the given <paramref name="providerType"/>.
         /// </summary>
-        /// <param name="providerType">The provider type, this is element under "Egress:" in configuration. You can use <see cref="ProviderTypes"/> to get valid strings to use here.</param>
+        /// <param name="providerType">The provider type, the element under "Egress:" in configuration. You can use <see cref="ProviderTypes"/> to get valid strings to use here.</param>
         IConfigurationSection GetConfigurationSection(string providerType);
 
         /// <summary>
         /// The configuration section that should be monitored for changes. This will return the minimum set of <see cref="IConfigurationSection"/> to monitor for changes.
-        /// Note: a change in this section does not guarentee a meaningful change to configuration.
+        /// Note: a change in this section does not guarantee a meaningful change to configuration.
         /// </summary>
         IConfigurationSection GetTokenChangeSourceSection();
     }
 
     /// <summary>
-    /// Provides access to all of the egress provider configuraiton blocks that use the given <see cref="OptionsType"/>.
-    /// These configuration blocks will be from "Egress:{ProviderType}" where "{ProviderType}" is one of the strings definded in <see cref="ProviderTypes"/>.
+    /// Provides access to all of the egress provider configuration blocks that use the given <see cref="OptionsType"/>.
+    /// These configuration blocks will be from "Egress:{ProviderType}" where "{ProviderType}" is one of the strings defined in <see cref="ProviderTypes"/>.
     /// This is the typed instance of <see cref="IEgressPropertiesConfigurationProvider"/>.
     /// </summary>
     internal interface IEgressProviderConfigurationProvider<TOptions> :
