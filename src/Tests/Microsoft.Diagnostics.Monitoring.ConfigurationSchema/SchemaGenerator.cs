b4214cc0c8d86da1fe57a9bb683aa5e3a76690e4
Wiktor Kopec <wiktork@microsoft.com>
2022-05-10 10:54:57 -0700

    Flatten hierarchy during schema generation to get rid of base types

--- src/Tests/Microsoft.Diagnostics.Monitoring.ConfigurationSchema/SchemaGenerator.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.ConfigurationSchema/SchemaGenerator.cs
@@ -269,6 +269,7 @@ namespace Microsoft.Diagnostics.Monitoring.ConfigurationSchema
                 Schema = rootSchema;
 
                 _settings = new JsonSchemaGeneratorSettings();
+                _settings.FlattenInheritanceHierarchy = true;
                 _settings.SerializerSettings = new JsonSerializerSettings();
                 _settings.SerializerSettings.Converters.Add(new StringEnumConverter());
 
