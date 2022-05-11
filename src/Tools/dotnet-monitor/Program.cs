fb30e1b2c6641b916efb890537ccb0c8a388b4df
kkeirstead <kkeirstead@microsoft.com>
2022-05-11 16:20:50 -0700

    Trying to update System CommandLine seems to have broken a lot of things - shelving to investigate

--- src/Tools/dotnet-monitor/Program.cs
+++ src/Tools/dotnet-monitor/Program.cs
@@ -10,47 +10,81 @@ using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
     class Program
     {
-        private static Command GenerateApiKeyCommand() =>
-            new Command(
+        private static Command GenerateApiKeyCommand()
+        {
+            Command command = new Command(
                 name: "generatekey",
-                description: Strings.HelpDescription_CommandGenerateKey)
+                description: Strings.HelpDescription_CommandGenerateKey);
+
+            var output = Output();
+
+            command.Add(output);
+
+            command.SetHandler((CancellationToken token, OutputFormat output, IConsole console) => GenerateApiKeyCommandHandler.Invoke(token, output, console), output);
+
+            return command;
+        }
+
+        private static Command CollectCommand()
+        {
+            var urls = Urls();
+            var metricUrls = MetricUrls();
+            var provideMetrics = ProvideMetrics();
+            var diagnosticPort = DiagnosticPort();
+            var noAuth = NoAuth();
+            var tempApiKey = TempApiKey();
+            var noHttpEgress = NoHttpEgress();
+
+            Command command = new Command(
+                name: "collect",
+                description: Strings.HelpDescription_CommandCollect)
             {
-                CommandHandler.Create(GenerateApiKeyCommandHandler.Invoke),
-                Output()
+                urls, metricUrls, provideMetrics, diagnosticPort, noAuth, tempApiKey, noHttpEgress
             };
 
+            //command.Add(SharedOptions());
+
+            //             Urls(), MetricUrls(), ProvideMetrics(), DiagnosticPort(), NoAuth(), TempApiKey(), NoHttpEgress()
+
+            command.SetHandler((CancellationToken token, string[] urls, string[] metricUrls, bool metrics, string diagnosticPort, bool noAuth, bool tempApiKey, bool noHttpEgress) => CollectCommandHandler.Invoke(token, urls, metricUrls, metrics, diagnosticPort, noAuth, tempApiKey, noHttpEgress), urls, metricUrls, provideMetrics, diagnosticPort, noAuth, tempApiKey, noHttpEgress);
+
+            return command;
+        }
+
+        /*
         private static Command CollectCommand() =>
             new Command(
                 name: "collect",
                 description: Strings.HelpDescription_CommandCollect)
             {
                 // Handler
-                CommandHandler.Create(CollectCommandHandler.Invoke),
+                CommandHandler.Create(CollectCommandHandler.Dummy),
                 SharedOptions()
             };
+        */
 
-        private static Command ConfigCommand() =>
-            new Command(
+        private static Command ConfigCommand()
+        {
+            // Doesn't have show
+            Command command = new Command(
                 name: "config",
-                description: Strings.HelpDescription_CommandConfig)
-            {
-                new Command(
-                    name: "show",
-                    description: Strings.HelpDescription_CommandShow)
-                {
-                    // Handler
-                    CommandHandler.Create(ConfigShowCommandHandler.Invoke),
-                    SharedOptions(),
-                    ConfigLevel(),
-                    ShowSources()
-                }
-            };
+                description: Strings.HelpDescription_CommandConfig);
+
+            command.Add(SharedOptions());
+            command.Add(ConfigLevel());
+            command.Add(ShowSources());
+
+            command.SetHandler((string[] urls, string[] metricUrls, bool metrics, string diagnosticPort, bool noAuth, bool tempApiKey, bool noHttpEgress, ConfigDisplayLevel level, bool showSources) => ConfigShowCommandHandler.Invoke(urls, metricUrls, metrics, diagnosticPort, noAuth, tempApiKey, noHttpEgress, level, showSources));
+
+            return command;
+        }
 
         private static IEnumerable<Option> SharedOptions() => new Option[]
         {
@@ -86,7 +120,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option DiagnosticPort() =>
             new Option<string>(
-                alias: "--diagnostic-port",
+                name: "--diagnostic-port",
                 description: Strings.HelpDescription_OptionDiagnosticPort)
             {
                 ArgumentHelpName = "diagnosticPort"
@@ -94,7 +128,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option NoAuth() =>
             new Option<bool>(
-                alias: "--no-auth",
+                name: "--no-auth",
                 description: Strings.HelpDescription_OptionNoAuth,
                 getDefaultValue: () => false)
             {
@@ -103,7 +137,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option NoHttpEgress() =>
             new Option<bool>(
-                alias: "--no-http-egress",
+                name: "--no-http-egress",
                 description: Strings.HelpDescription_OptionNoHttpEgress,
                 getDefaultValue: () => false)
             {
@@ -112,7 +146,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option TempApiKey() =>
             new Option<bool>(
-                alias: "--temp-apikey",
+                name: "--temp-apikey",
                 description: Strings.HelpDescription_OptionTempApiKey,
                 getDefaultValue: () => false)
             {
@@ -130,7 +164,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option ConfigLevel() =>
             new Option<ConfigDisplayLevel>(
-                alias: "--level",
+                name: "--level",
                 description: Strings.HelpDescription_OptionLevel,
                 getDefaultValue: () => ConfigDisplayLevel.Redacted)
             {
@@ -139,7 +173,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         private static Option ShowSources() =>
             new Option<bool>(
-                alias: "--show-sources",
+                name: "--show-sources",
                 description: Strings.HelpDescription_OptionShowSources,
                 getDefaultValue: () => false)
             {
@@ -148,12 +182,15 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         public static Task<int> Main(string[] args)
         {
-            var parser = new CommandLineBuilder()
-                .AddCommand(CollectCommand())
-                .AddCommand(ConfigCommand())
-                .AddCommand(GenerateApiKeyCommand())
-                .UseDefaults()
-                .Build();
+            var parser = new CommandLineBuilder(new RootCommand()
+            {
+                CollectCommand(),
+                ConfigCommand(),
+                GenerateApiKeyCommand()
+            })
+            .UseDefaults()
+            .Build();
+
             return parser.InvokeAsync(args);
         }
     }
