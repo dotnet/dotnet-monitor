533292d722f0aac30df16c217ae55c2cc4ac880a
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2022-06-23 17:06:51 -0400

    Removed resx strings that weren't merged correctly

--- src/Tools/dotnet-monitor/Strings.Designer.cs
+++ src/Tools/dotnet-monitor/Strings.Designer.cs
@@ -880,6 +880,15 @@ namespace Microsoft.Diagnostics.Tools.Monitor {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The intermediate file at the following path could not be deleted: {path}.
+        /// </summary>
+        internal static string LogFormatString_IntermediateFileDeletionFailed {
+            get {
+                return ResourceManager.GetString("LogFormatString_IntermediateFileDeletionFailed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Invalid action reference &apos;{actionReference}&apos;..
         /// </summary>
         internal static string LogFormatString_InvalidActionReference {
@@ -952,11 +961,50 @@ namespace Microsoft.Diagnostics.Tools.Monitor {
         }
         
         /// <summary>
+<<<<<<< HEAD
+=======
+        ///   Looks up a localized string similar to The provided url could not be parsed: {url}.
+        /// </summary>
+        internal static string LogFormatString_ParsingUrlFailed {
+            get {
+                return ResourceManager.GetString("LogFormatString_ParsingUrlFailed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The queue {0} does not exist; ensure that the {queueName} and {queueAccountUri} fields are set correctly..
+        /// </summary>
+        internal static string LogFormatString_QueueDoesNotExist {
+            get {
+                return ResourceManager.GetString("LogFormatString_QueueDoesNotExist", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Queue message egress requires {queueName} and {queueAccountUri} to be set.
+        /// </summary>
+        internal static string LogFormatString_QueueOptionsPartiallySet {
+            get {
+                return ResourceManager.GetString("LogFormatString_QueueOptionsPartiallySet", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+>>>>>>> 6159f811 (Address "Swallow Everything" Exceptions (#2022))
         ///   Looks up a localized string similar to The process was launched elevated and will have access to all processes on the system. Do not run elevated unless you need to monitor processes launched by another user (e.g., IIS worker processes).
         /// </summary>
         internal static string LogFormatString_RunningElevated {
             get {
                 return ResourceManager.GetString("LogFormatString_RunningElevated", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Unable to get the runtime instance cookie of the current process..
+        /// </summary>
+        internal static string LogFormatString_RuntimeInstanceCookieFailedToFilterSelf {
+            get {
+                return ResourceManager.GetString("LogFormatString_RuntimeInstanceCookieFailedToFilterSelf", resourceCulture);
             }
         }
         
