a44486acee6367813c8d6aa8c4dcffa7332391db
kkeirstead <kkeirstead@microsoft.com>
2022-06-27 11:41:23 -0700

    Adding in some basic infrastructure and tests

--- src/Tools/dotnet-monitor/CollectionRules/KnownCollectionRuleActions.cs
+++ src/Tools/dotnet-monitor/CollectionRules/KnownCollectionRuleActions.cs
@@ -10,6 +10,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor.CollectionRules
         public const string CollectGCDump = nameof(CollectGCDump);
         public const string CollectLogs = nameof(CollectLogs);
         public const string CollectTrace = nameof(CollectTrace);
+        public const string CollectLiveMetrics = nameof(CollectLiveMetrics);
         public const string Execute = nameof(Execute);
         public const string LoadProfiler = nameof(LoadProfiler);
         public const string SetEnvironmentVariable = nameof(SetEnvironmentVariable);
---
+++ src/Tools/dotnet-monitor/CollectionRules/Options/Actions/CollectLiveMetricsOptions.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Diagnostics.Monitoring.Options;
+using Microsoft.Diagnostics.Monitoring.WebApi;
+using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options.CollectionRuleDefaultsInterfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options.Actions
+{
+    /// <summary>
+    /// Options for the CollectLiveMetrics action.
+    /// </summary>
+    [DebuggerDisplay("CollectLiveMetrics")]
+#if SCHEMAGEN
+    [NJsonSchema.Annotations.JsonSchemaFlatten]
+#endif
+    internal sealed partial record class CollectLiveMetricsOptions : BaseRecordOptions, IEgressProviderProperties
+    {
+        [Display(
+            ResourceType = typeof(OptionsDisplayStrings),
+            Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectLogsOptions_Duration))]
+        [Range(typeof(TimeSpan), ActionOptionsConstants.Duration_MinValue, ActionOptionsConstants.Duration_MaxValue)]
+        [DefaultValue(CollectLogsOptionsDefaults.Duration)]
+        public TimeSpan? Duration { get; set; }
+
+        [Display(
+            ResourceType = typeof(OptionsDisplayStrings),
+            Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectLogsOptions_Egress))]
+        [Required(
+            ErrorMessageResourceType = typeof(OptionsDisplayStrings),
+            ErrorMessageResourceName = nameof(OptionsDisplayStrings.ErrorMessage_NoDefaultEgressProvider))]
+#if !UNITTEST && !SCHEMAGEN
+        [ValidateEgressProvider]
+#endif
+        public string Egress { get; set; }
+    }
+}
