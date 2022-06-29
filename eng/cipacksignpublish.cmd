609707bec25b08299955db91221cf40f7a5ace1f
Justin Anderson <jander@microsoft.com>
2022-06-29 08:59:56 -0700

    Allow rebuild during packaging

--- eng/cipacksignpublish.cmd
+++ eng/cipacksignpublish.cmd
@@ -4,5 +4,5 @@ setlocal
 set "_commonArgs=-restore -ci -prepareMachine -verbosity minimal -configuration Release"
 set "_logDir=%~dp0..\artifacts\log\Release\"
 
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0common\Build.ps1""" %_commonArgs% -pack -sign -publish /p:NoBuild=true -noBl /bl:'%_logDir%PackSignPublish.binlog' %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0common\Build.ps1""" %_commonArgs% -pack -sign -publish -noBl /bl:'%_logDir%PackSignPublish.binlog' %*"
 exit /b %ERRORLEVEL%
