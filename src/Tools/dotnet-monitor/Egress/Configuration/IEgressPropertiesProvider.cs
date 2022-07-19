604cecfdee978e238dbf1f33fad9cbc850dfab3f
kkeirstead <kkeirstead@microsoft.com>
2022-07-19 12:05:52 -0700

    Typo in comments

--- src/Tools/dotnet-monitor/Egress/Configuration/IEgressPropertiesProvider.cs
+++ src/Tools/dotnet-monitor/Egress/Configuration/IEgressPropertiesProvider.cs
@@ -12,9 +12,9 @@ namespace Microsoft.Diagnostics.Tools.Monitor.Egress.Configuration
     internal interface IEgressPropertiesProvider
     {
         /// <summary>
-        /// Gets the set of keys defined as a <see cref="IDictionary{strings, string}"/> with values populated.
+        /// Gets the set of keys defined as a <see cref="IDictionary{string, string}"/> with values populated.
         /// </summary>
-        /// <returns><see cref="IDictionary{strings, string}"/> representing the set of properties.</returns>
+        /// <returns><see cref="IDictionary{string, string}"/> representing the set of properties.</returns>
         IDictionary<string, string> GetAllProperties();
 
         /// <summary>
