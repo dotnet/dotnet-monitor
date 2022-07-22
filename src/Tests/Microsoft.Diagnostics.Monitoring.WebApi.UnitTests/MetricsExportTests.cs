d11b2a9ac4e0e393775b96e4039eab10e2353132
roman.bats <roman.bats@advancedmd.com>
2022-07-22 12:22:44 +0300

    Remove redundant test data

--- src/Tests/Microsoft.Diagnostics.Monitoring.WebApi.UnitTests/MetricsExportTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.WebApi.UnitTests/MetricsExportTests.cs
@@ -50,41 +50,22 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi.UnitTests
         }
 
         [Theory()]
-        [InlineData("System.Runtime", "assembly-count", "", 225, "225")]
-        [InlineData("System.Runtime", "gc-committed", "MB", 9, "9000000")]
-        [InlineData("System.Runtime", "gen-0-size", "B", 48, "48")]
-        [InlineData("System.Runtime", "time-in-jit", "ms", 0, "0")]
-        [InlineData("Microsoft.AspNetCore.Hosting", "current-requests", "", 4, "4")]
-        [InlineData("System.Runtime", "gen-0-gc-count", "count", 10, "10")]
-        [InlineData("Microsoft.AspNetCore.Hosting", "requests-per-second", "count", 0, "0")]
-        [InlineData("System.Runtime", "cpu-usage", "%", 2, "2")]
-        [InlineData("System.Runtime", "poh-size", "B", 178936, "178936")]
-        [InlineData("System.Runtime", "threadpool-queue-length", "", 0, "0")]
-        [InlineData("Microsoft.AspNetCore.Hosting", "failed-requests", "", 0, "0")]
-        [InlineData("Microsoft.AspNetCore.Hosting", "total-requests", "", 2, "2")]
-        [InlineData("System.Runtime", "gc-fragmentation", "%", 0.691783039570667, "0.691783039570667")]
-        [InlineData("System.Runtime", "active-timer-count", "", 3, "3")]
-        [InlineData("System.Runtime", "monitor-lock-contention-count", "count", 2, "2")]
-        [InlineData("System.Runtime", "methods-jitted-count", "", 4540, "4540")]
-        [InlineData("System.Runtime", "gen-2-size", "B", 48, "48")]
-        [InlineData("System.Runtime", "il-bytes-jitted", "B", 461142, "461142")]
-        [InlineData("System.Runtime", "threadpool-thread-count", "", 4, "4")]
-        [InlineData("System.Runtime", "loh-size", "B", 1874968, "1874968")]
-        [InlineData("System.Runtime", "gen-1-size", "B", 1726376, "1726376")]
-        [InlineData("System.Runtime", "threadpool-completed-items-count", "count", 2, "2")]
-        [InlineData("System.Runtime", "working-set", "MB", 112, "112000000")]
-        [InlineData("System.Runtime", "gen-1-gc-count", "count", 340, "340")]
-        [InlineData("System.Runtime", "gen-2-gc-count", "count", 41, "41")]
-        [InlineData("System.Runtime", "time-in-gc", "%", 0, "0")]
-        [InlineData("System.Runtime", "alloc-rate", "B", 0, "0")]
-        [InlineData("System.Runtime", "gc-heap-size", "MB", 18, "18000000")]
-        [InlineData("System.Runtime", "exception-count", "count", 0, "0")]
-        public void TestPrometheusNormalizationValue(string metricProvider, string metricName, string metricUnit, double metricValue, string expectedValue)
+        [InlineData("", 225, "225")]
+        [InlineData("MB", 9, "9000000")]
+        [InlineData("mb", 112, "112000000")]
+        [InlineData("B", 48, "48")]
+        [InlineData("B", 178936, "178936")]
+        [InlineData("b", 461142, "461142")]
+        [InlineData("ms", 0, "0")]
+        [InlineData("", 4, "4")]
+        [InlineData("count", 10, "10")]
+        [InlineData("%", 2, "2")]
+        [InlineData("%", 0.691783039570667, "0.691783039570667")]
+        [InlineData("%", 0, "0")]
+        public void TestPrometheusNormalizationValue(string metricUnit, double metricValue, string expectedValue)
         {
             var normalizedValue = PrometheusDataModel.GetPrometheusNormalizedValue(metricUnit, metricValue);
             Assert.Equal(normalizedValue, expectedValue);
-            Assert.NotEmpty(metricProvider);
-            Assert.NotEmpty(metricName);
         }
     }
 }
