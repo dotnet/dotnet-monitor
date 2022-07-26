3b00b7f4930f74353f128fec42c7733f38042779
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2022-07-26 14:58:15 -0700

    Relint

--- src/Tools/dotnet-monitor/HostBuilderHelper.cs
+++ src/Tools/dotnet-monitor/HostBuilderHelper.cs
@@ -181,7 +181,8 @@ namespace Microsoft.Diagnostics.Tools.Monitor
         private static void ConfigureEndpointInfoSource(IConfigurationBuilder builder, string diagnosticPort)
         {
             DiagnosticPortConnectionMode connectionMode = GetConnectionMode(diagnosticPort);
-            builder.AddInMemoryCollection(new Dictionary<string, string> {
+            builder.AddInMemoryCollection(new Dictionary<string, string>
+            {
                 {ConfigurationPath.Combine(ConfigurationKeys.DiagnosticPort, nameof(DiagnosticPortOptions.ConnectionMode)), connectionMode.ToString()},
                 {ConfigurationPath.Combine(ConfigurationKeys.DiagnosticPort, nameof(DiagnosticPortOptions.EndpointName)), diagnosticPort}
             });
