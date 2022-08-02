6c1d890f83a5e165785c196da7e67f5ba9db2c6f
Justin Anderson <jander@microsoft.com>
2022-08-02 14:37:43 -0700

    Remove .NET 5 testing

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/ActionTestsHelper.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/ActionTestsHelper.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
 {
     internal static class ActionTestsHelper
     {
-        public static TargetFrameworkMoniker[] tfmsToTest = new TargetFrameworkMoniker[] { TargetFrameworkMoniker.Net50, TargetFrameworkMoniker.Net60 };
+        public static TargetFrameworkMoniker[] tfmsToTest = new TargetFrameworkMoniker[] { TargetFrameworkMoniker.Net60 };
         public static TargetFrameworkMoniker[] tfms6PlusToTest = new TargetFrameworkMoniker[] { TargetFrameworkMoniker.Net60 };
 
         public static IEnumerable<object[]> GetTfms()
