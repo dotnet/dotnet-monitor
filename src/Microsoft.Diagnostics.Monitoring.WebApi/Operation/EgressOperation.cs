e9ef4387cff3ed776def822bbeaf22c57a387edf
Justin Anderson <jander-msft@users.noreply.github.com>
2022-08-05 10:45:07 -0700

    Update src/Microsoft.Diagnostics.Monitoring.WebApi/Operation/EgressOperation.cs
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Microsoft.Diagnostics.Monitoring.WebApi/Operation/EgressOperation.cs
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/Operation/EgressOperation.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Diagnostics.Monitoring.WebApi.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
