bded94ba65d175126d755b2415b31c8ecbb6c965
Justin Anderson <jander-msft@users.noreply.github.com>
2022-09-06 14:04:18 -0700

    Disable exception tracking via profiler (#2452)

--- src/Tests/Microsoft.Diagnostics.Monitoring.Profiler.UnitTests/ExceptionTrackingTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Profiler.UnitTests/ExceptionTrackingTests.cs
@@ -33,14 +33,14 @@ namespace Microsoft.Diagnostics.Monitoring.Profiler.UnitTests
             _outputHelper = outputHelper;
         }
 
-        [Theory]
+        [Theory(Skip = "Exception tracking via profiler is currently disabled")]
         [MemberData(nameof(ProfilerHelper.GetArchitectureProfilerPath), MemberType = typeof(ProfilerHelper))]
         public Task ExceptionThrowCatch(Architecture architecture, string profilerPath)
         {
             return RunAndCompare(nameof(ExceptionThrowCatch), architecture, profilerPath);
         }
 
-        [Theory]
+        [Theory(Skip = "Exception tracking via profiler is currently disabled")]
         [MemberData(nameof(ProfilerHelper.GetArchitectureProfilerPath), MemberType = typeof(ProfilerHelper))]
         public Task ExceptionThrowCrash(Architecture architecture, string profilerPath)
         {
