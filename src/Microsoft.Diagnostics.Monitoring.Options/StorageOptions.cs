3581c6d152ad04245ff757b3df4ba957f0a173e8
Justin Anderson <jander@microsoft.com>
2022-09-20 14:07:33 -0700

    Prepend experimental prefix to experimental options

--- src/Microsoft.Diagnostics.Monitoring.Options/StorageOptions.cs
+++ src/Microsoft.Diagnostics.Monitoring.Options/StorageOptions.cs
@@ -2,12 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Diagnostics.Monitoring.Options;
 using System.ComponentModel.DataAnnotations;
 
 namespace Microsoft.Diagnostics.Monitoring.WebApi
 {
     internal class StorageOptions
     {
+        [Experimental]
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_StorageOptions_DefaultSharedPath))]
@@ -18,6 +20,7 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_StorageOptions_DumpTempFolder))]
         public string DumpTempFolder { get; set; }
 
+        [Experimental]
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_StorageOptions_SharedLibraryPath))]
