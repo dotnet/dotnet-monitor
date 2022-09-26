ef7280632a3781eed4f236635881be6aed8ba409
Justin Anderson <jander-msft@users.noreply.github.com>
2022-09-26 15:14:34 -0700

    Unwrap ChainedConfigurationProvider to get real provider

---
+++ src/Tools/dotnet-monitor/ConfigurationExtensions.cs
@@ -0,0 +1,66 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Diagnostics.Tools.Monitor
+{
+    internal static class ConfigurationExtensions
+    {
+        public static bool TryGetProviderAndValue(this IConfiguration configuration, string key, out IConfigurationProvider provider, out string value)
+        {
+            if (configuration is IConfigurationRoot configurationRoot)
+            {
+                return configurationRoot.TryGetProviderAndValue(key, out provider, out value);
+            }
+
+            provider = null;
+            value = null;
+            return false;
+        }
+
+        public static bool TryGetProviderAndValue(this IConfigurationRoot configurationRoot, string key, out IConfigurationProvider provider, out string value)
+        {
+            foreach (IConfigurationProvider candidate in configurationRoot.Providers.Reverse())
+            {
+                if (candidate is ChainedConfigurationProvider chainedProvider &&
+                    chainedProvider.TryGetInnerConfiguration(out IConfiguration innerConfiguration) &&
+                    innerConfiguration.TryGetProviderAndValue(key, out provider, out value))
+                {
+                    return true;
+                }
+
+                if (candidate.TryGet(key, out value))
+                {
+                    provider = candidate;
+                    return true;
+                }
+            }
+
+            provider = null;
+            value = null;
+            return false;
+        }
+
+        private static bool TryGetInnerConfiguration(this ChainedConfigurationProvider provider, out IConfiguration configuration)
+        {
+#if NET7_0_OR_GREATER
+            configuration = provider.Configuration;
+            return true;
+#else
+            FieldInfo configField = provider.GetType().GetField("_config", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (null != configField)
+            {
+                configuration = configField.GetValue(provider) as IConfiguration;
+                return null != configuration;
+            }
+
+            configuration = null;
+            return false;
+#endif
+        }
+    }
+}
