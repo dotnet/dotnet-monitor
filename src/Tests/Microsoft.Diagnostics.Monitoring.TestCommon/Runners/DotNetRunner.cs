0aded3fd08bfcdd257e048b53452e2331a69c86c
Justin Anderson <jander-msft@users.noreply.github.com>
2022-10-05 16:07:40 -0700

    Run apps with test frameworks by using customized runtimeconfig.json

--- src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/Runners/DotNetRunner.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/Runners/DotNetRunner.cs
@@ -58,11 +58,6 @@ namespace Microsoft.Diagnostics.Monitoring.TestCommon.Runners
         public Task<int> ExitedTask => _exitedSource.Task;
 
         /// <summary>
-        /// The framework reference of the app to run.
-        /// </summary>
-        public DotNetFrameworkReference FrameworkReference { get; set; } = DotNetFrameworkReference.Microsoft_NetCore_App;
-
-        /// <summary>
         /// Determines if the process has exited.
         /// </summary>
         public bool HasExited => HasStarted && _process.HasExited;
@@ -88,11 +83,6 @@ namespace Microsoft.Diagnostics.Monitoring.TestCommon.Runners
         public StreamReader StandardOutput => _process.StandardOutput;
 
         /// <summary>
-        /// Get or set the target framework on which the application should run.
-        /// </summary>
-        public TargetFrameworkMoniker TargetFramework { get; set; } = TargetFrameworkMoniker.Current;
-
-        /// <summary>
         /// Determines if <see cref="StartAsync(CancellationToken)" /> should wait for the diagnostic pipe to be available.
         /// </summary>
         public bool WaitForDiagnosticPipe { get; set; }
@@ -100,7 +90,7 @@ namespace Microsoft.Diagnostics.Monitoring.TestCommon.Runners
         public DotNetRunner()
         {
             _process = new Process();
-            _process.StartInfo.FileName = DotNetHost.HostExePath;
+            _process.StartInfo.FileName = DotNetHost.GetPath();
             _process.StartInfo.UseShellExecute = false;
             _process.StartInfo.RedirectStandardError = true;
             _process.StartInfo.RedirectStandardInput = true;
@@ -125,33 +115,12 @@ namespace Microsoft.Diagnostics.Monitoring.TestCommon.Runners
         /// </summary>
         public async Task StartAsync(CancellationToken token)
         {
-            string frameworkVersion = null;
-            switch (FrameworkReference)
-            {
-                case DotNetFrameworkReference.Microsoft_AspNetCore_App:
-                    // Starting in .NET 6, the .NET SDK is emitting two framework references
-                    // into the .runtimeconfig.json file. This is preventing the --fx-version
-                    // parameter from having the correct effect of using the exact framework version
-                    // that we want. Disabling this forced version usage for ASP.NET 6+ applications
-                    // until it can be resolved.
-                    if (!TargetFramework.IsEffectively(TargetFrameworkMoniker.Net60))
-                    {
-                        frameworkVersion = TargetFramework.GetAspNetCoreFrameworkVersionString();
-                    }
-                    break;
-                case DotNetFrameworkReference.Microsoft_NetCore_App:
-                    frameworkVersion = TargetFramework.GetNetCoreAppFrameworkVersionString();
-                    break;
-                default:
-                    throw new InvalidOperationException($"Unsupported framework reference: {FrameworkReference}");
-            }
-
             StringBuilder argsBuilder = new();
-            if (!string.IsNullOrEmpty(frameworkVersion))
+            if (DotNetHost.HasHostInRepository)
             {
-                argsBuilder.Append("--fx-version ");
-                argsBuilder.Append(frameworkVersion);
-                argsBuilder.Append(" ");
+                argsBuilder.Append("exec --runtimeconfig \"");
+                argsBuilder.Append(Path.ChangeExtension(EntrypointAssemblyPath, ".runtimeconfig.test.json"));
+                argsBuilder.Append("\" ");
             }
             argsBuilder.Append("\"");
             argsBuilder.Append(EntrypointAssemblyPath);
