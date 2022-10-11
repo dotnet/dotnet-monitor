d899ffe4c9dfae56e1f45f531dd213fbe7462bf4
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2022-10-11 10:02:14 -0700

    Update src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
@@ -3,9 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Diagnostics.Tracing;
-using Microsoft.FileFormats;
-using Microsoft.FileFormats.ELF;
-using Microsoft.FileFormats.MachO;
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
