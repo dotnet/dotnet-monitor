3a9bd04f59640ee2d96835db78d180ffd8ccad06
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2022-10-19 09:06:33 -0700

    Update src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TraceTestUtilities.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Diagnostics.Tracing;
-using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
