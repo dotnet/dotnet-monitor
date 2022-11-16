72f3c90fc8304e70a6304a9ed55018109fea910e
kkeirstead <kkeirstead@microsoft.com>
2022-11-16 09:33:56 -0800

    Working on tests

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/MetricsFormattingTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/MetricsFormattingTests.cs
@@ -2,21 +2,39 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options;
+using Microsoft.Diagnostics.Monitoring.TestCommon;
+using Microsoft.Diagnostics.Monitoring.WebApi;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
 {
-    internal static class LimitsTestsHelper
+    public sealed class MetricsFormattingTests
     {
-        internal static CollectionRuleLimitsOptions GetLimitsOptions(IHost host, string ruleName)
-        {
-            IOptionsMonitor<CollectionRuleOptions> ruleOptionsMonitor = host.Services.GetService<IOptionsMonitor<CollectionRuleOptions>>();
-            CollectionRuleLimitsOptions options = ruleOptionsMonitor.Get(ruleName).Limits;
+        private ITestOutputHelper _outputHelper;
 
-            return options;
+        public MetricsFormattingTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public async Task HistogramFormat_Test()
+        {
+            await TestHostHelper.CreateCollectionRulesHost(_outputHelper, rootOptions => { }, host =>
+            {
+                IOptionsMonitor<DiagnosticPortOptions> options = host.Services.GetService<IOptionsMonitor<DiagnosticPortOptions>>();
+
+                Assert.Equal(DiagnosticPortTestsConstants.SimplifiedDiagnosticPort, options.CurrentValue.EndpointName);
+                Assert.Equal(DiagnosticPortConnectionMode.Listen, options.CurrentValue.ConnectionMode);
+
+            }, overrideSource: GetConfigurationSources(DiagnosticPortTestsConstants.SimplifiedListen_EnvironmentVariables));
         }
     }
 }
