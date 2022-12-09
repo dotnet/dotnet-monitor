cc67873315dc66a7b87cd2fc6e88f604843bbcbe
Justin Anderson <jander-msft@users.noreply.github.com>
2022-12-09 15:36:48 -0800

    Standardize on two-line license header (#3066)

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -83,7 +83,6 @@ Please use the following file header for new files.
 ```
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 ```
 
 ### PR - CI Process
--- src/Tools/dotnet-monitor/Auth/JwtBearerPostConfigure.cs
+++ src/Tools/dotnet-monitor/Auth/JwtBearerPostConfigure.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
