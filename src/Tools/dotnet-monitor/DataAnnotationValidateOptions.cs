7bbc729f40c9ddc682007c8cd49fed7ebde9e85f
Justin Anderson <jander-msft@users.noreply.github.com>
2022-12-09 15:36:48 -0800

    Standardize on two-line license header

--- src/Tools/dotnet-monitor/DataAnnotationValidateOptions.cs
+++ src/Tools/dotnet-monitor/DataAnnotationValidateOptions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Options;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
