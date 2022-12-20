b282b56cb98888b80a1c8174787816306c92133c
Justin Anderson <jander-msft@users.noreply.github.com>
2022-12-20 14:40:12 -0800

    Run collection rule action tests serially (#3145)

---
+++ src/Tests/CollectionRuleActions.UnitTests/TestCollections.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace CollectionRuleActions.UnitTests
+{
+    internal static class TestCollections
+    {
+        // This test collection is used to force all of the collection rule action tests
+        // to be invoked serially in order to avoid thread pool exhaustion issues that
+        // impact the ability for the collection rule actions to start asynchronously in
+        // a reasonable amount of time.
+        public const string CollectionRuleActions = nameof(CollectionRuleActions);
+    }
+}
