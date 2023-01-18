e3225c301aa23a7e3c4a9c2d77f0b89f51c64d14
Sam Spencer <54915162+samsp-msft@users.noreply.github.com>
2023-01-18 09:39:53 -0800

    Swagger UI (#3241)
    Co-authored-by: Sam Spencer <samsp@microsoft.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Justin Anderson <jander-msft@users.noreply.github.com>

--- src/Tools/dotnet-monitor/Strings.Designer.cs
+++ src/Tools/dotnet-monitor/Strings.Designer.cs
@@ -529,6 +529,15 @@ namespace Microsoft.Diagnostics.Tools.Monitor {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to JWT Authorization header using the Bearer scheme. Put only the JWT Token in the textbox when prompted using Swagger UI..
+        /// </summary>
+        internal static string HelpDescription_JWT_Header {
+            get {
+                return ResourceManager.GetString("HelpDescription_JWT_Header", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The fully qualified path and filename of the json configuration file you&apos;d like to add to the list of configuration sources..
         /// </summary>
         internal static string HelpDescription_OptionConfigurationFilePath {
