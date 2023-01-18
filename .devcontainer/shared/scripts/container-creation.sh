13cb1a0d903cf1c1b89fca87c7277e7107f9d195
Joe Schmitt <joschmit@microsoft.com>
2023-01-18 22:33:43 +0000

    Remove sudo

--- .devcontainer/shared/scripts/container-creation.sh
+++ .devcontainer/shared/scripts/container-creation.sh
@@ -14,7 +14,7 @@ dotnet dev-certs https
 # Install ytt
 shasum -a 256 -c ./.devcontainer/shared/ytt-linux-amd64.sha
 chmod +x ./ytt-linux-amd64
-sudo mv ./ytt-linux-amd64 /usr/local/bin/ytt
+mv ./ytt-linux-amd64 /usr/local/bin/ytt
 
 # The container creation script is executed in a new Bash instance
 # so we exit at the end to avoid the creation process lingering.
