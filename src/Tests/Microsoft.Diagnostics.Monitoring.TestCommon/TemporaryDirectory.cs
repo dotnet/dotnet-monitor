06a4b7475100f1f2bde21b4ab422b55e52441ef6
Joe Schmitt <joschmit@microsoft.com>
2023-01-24 09:48:32 -0800

    Use agent's temp dir

--- src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TemporaryDirectory.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.TestCommon/TemporaryDirectory.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.Diagnostics.Monitoring.TestCommon
@@ -16,9 +17,18 @@ namespace Microsoft.Diagnostics.Monitoring.TestCommon
         {
             _outputHelper = outputhelper;
 
-            _directoryInfo = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
-            _directoryInfo.Create();
+            // AGENT_TEMPDIRECTORY is AzureDevops variable which is set to a path
+            // that is cleaned up after every job.
+            string topLevelTempDir = Environment.GetEnvironmentVariable("AGENT_TEMPDIRECTORY");
+            if (string.IsNullOrEmpty(topLevelTempDir))
+            {
+                topLevelTempDir = Path.GetTempPath();
+            }
 
+            string tempDir = Path.Combine(topLevelTempDir, Path.GetRandomFileName());
+            Assert.False(Directory.Exists(tempDir));
+
+            _directoryInfo = Directory.CreateDirectory(Path.Combine(topLevelTempDir, Path.GetRandomFileName()));
             _outputHelper.WriteLine("Created temporary directory '{0}'", FullName);
         }
 
