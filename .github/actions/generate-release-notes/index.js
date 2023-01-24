2929cd617c2f04692e5e5671975f419b41a1cc77
Joe Schmitt <joschmit@microsoft.com>
2023-01-24 21:11:59 +0000

    Fix recursive backports

--- .github/actions/generate-release-notes/index.js
+++ .github/actions/generate-release-notes/index.js
@@ -216,7 +216,7 @@ async function resolveBackportPrToReleaseNotePr(octokit, pr, repoOwner, repoName
 
     if (originIsBackport) {
         if (maxRecursion > 0) {
-            return await resolveBackportPr(octokit, originPr, repoOwner, repoName, minMergeDate, maxRecursion - 1);
+            return await resolveBackportPrToReleaseNotePr(octokit, originPr, repoOwner, repoName, minMergeDate, maxRecursion - 1);
         }
     }
 
