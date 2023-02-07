b5d6cd73b087b08bed2fe7e1702f12adb3cbc78e
Joe Schmitt <joschmit@microsoft.com>
2023-02-07 23:46:46 +0000

    Test repo

--- .github/actions/generate-release-notes/index.js
+++ .github/actions/generate-release-notes/index.js
@@ -25,8 +25,8 @@ async function run() {
     const lastReleaseDate = core.getInput("last_release_date", { required: true });
     const branch = core.getInput("branch_name", { required: true });
 
-    const repoOwner = github.context.payload.repository.owner.login;
-    const repoName = github.context.payload.repository.name;
+    const repoOwner = "dotnet"
+    const repoName = "dotnet-monitor"
 
     try {
         const significantLabels = [
