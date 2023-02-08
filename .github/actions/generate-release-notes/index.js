33d16685c0de229e1690786fb55eca41654de12c
Joe Schmitt <joschmit@microsoft.com>
2023-02-08 00:34:43 +0000

    Cleanup log

--- .github/actions/generate-release-notes/index.js
+++ .github/actions/generate-release-notes/index.js
@@ -115,7 +115,7 @@ async function getPrsToMention(octokit, branch, repoOwner, repoName, minMergeDat
 
         let originNumber = pr.originNumber ?? pr.number;
         if (commitHashesInRelease.has(fqPr.merge_commit_sha) && !mentionedUrls.has(originNumber)) {
-            console.log(`Including: #${fqPr.number} -- origin:$${originNumber}`);
+            console.log(`Including: #${fqPr.number} -- origin:#${originNumber}`);
             mentionedOriginNumbers.add(originNumber);
             prs.push(pr);
         } else {
