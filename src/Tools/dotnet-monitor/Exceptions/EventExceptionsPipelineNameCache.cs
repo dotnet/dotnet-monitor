0e611478b67766305b46a94b76657d5aca6d36db
Justin Anderson <jander@microsoft.com>
2023-02-17 14:22:49 -0800

    Sort namespaces

--- src/Tools/dotnet-monitor/Exceptions/EventExceptionsPipelineNameCache.cs
+++ src/Tools/dotnet-monitor/Exceptions/EventExceptionsPipelineNameCache.cs
@@ -1,10 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
-using System;
-using Microsoft.Diagnostics.Monitoring.WebApi.Stacks;
 using Microsoft.Diagnostics.Monitoring.WebApi.Exceptions;
+using Microsoft.Diagnostics.Monitoring.WebApi.Stacks;
+using System;
+using System.Collections.Generic;
 
 namespace Microsoft.Diagnostics.Tools.Monitor.Exceptions
 {
