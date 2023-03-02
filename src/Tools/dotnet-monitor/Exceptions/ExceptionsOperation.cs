ea438e98e4f32c633a3896487643c47b16db762e
Justin Anderson <jander@microsoft.com>
2023-03-01 21:22:39 -0800

    Sort usings

--- src/Tools/dotnet-monitor/Exceptions/ExceptionsOperation.cs
+++ src/Tools/dotnet-monitor/Exceptions/ExceptionsOperation.cs
@@ -1,15 +1,15 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Threading;
-using System;
 using Microsoft.Diagnostics.Monitoring;
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using Microsoft.Diagnostics.Monitoring.WebApi.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Diagnostics.Tools.Monitor.Exceptions
 {
