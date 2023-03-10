2f51c65bf253b735ccf7c1942dca428706caed66
kkeirstead <kkeirstead@microsoft.com>
2023-03-10 10:26:27 -0800

    Switching back to only using .net 7 tfm for the problematic tests.

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/CollectionRulePipelineTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/CollectionRulePipelineTests.cs
@@ -164,7 +164,7 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
         /// Test that the pipeline works with the SystemDiagnosticsMetrics trigger (gauge instrument).
         /// </summary>
         [Theory]
-        [MemberData(nameof(GetTfmsSupportingPortListener))]
+        [InlineData(TargetFrameworkMoniker.Net70)]
         public Task CollectionRulePipeline_SystemDiagnosticsMetricsTriggerTest_Gauge(TargetFrameworkMoniker appTfm)
         {
             CallbackActionService callbackService = new(_outputHelper);
@@ -230,7 +230,7 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
         /// Test that the pipeline works with the SystemDiagnosticsMetrics trigger greater-than (histogram instrument).
         /// </summary>
         [Theory]
-        [MemberData(nameof(GetTfmsSupportingPortListener))]
+        [InlineData(TargetFrameworkMoniker.Net70)]
         public Task CollectionRulePipeline_SystemDiagnosticsMetricsTriggerTest_Histogram_GreaterThan(TargetFrameworkMoniker appTfm)
         {
             CallbackActionService callbackService = new(_outputHelper);
@@ -297,7 +297,7 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.UnitTests
         /// Test that the pipeline works with the SystemDiagnosticsMetrics trigger less-than (histogram instrument).
         /// </summary>
         [Theory]
-        [MemberData(nameof(GetTfmsSupportingPortListener))]
+        [InlineData(TargetFrameworkMoniker.Net70)]
         public Task CollectionRulePipeline_SystemDiagnosticsMetricsTriggerTest_Histogram_LessThan(TargetFrameworkMoniker appTfm)
         {
             CallbackActionService callbackService = new(_outputHelper);
