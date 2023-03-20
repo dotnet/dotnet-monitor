61f0f838fc346dd6847ffc5b223e96b06bfd8005
Justin Anderson <jander@microsoft.com>
2023-03-20 09:13:18 -0700

    Adjustments for API breaking changes

--- src/Tools/dotnet-monitor/Program.cs
+++ src/Tools/dotnet-monitor/Program.cs
@@ -19,10 +19,9 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                 OutputOption
             };
 
-            command.SetHandler((context, token) =>
+            command.SetAction((context) =>
             {
-                return GenerateApiKeyCommandHandler.Invoke(
-                    token,
+                GenerateApiKeyCommandHandler.Invoke(
                     context.ParseResult.GetValue(OutputOption),
                     context.Console);
             });
@@ -46,7 +45,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                 ConfigurationFilePathOption
             };
 
-            command.SetHandler((context, token) =>
+            command.SetActionWithExitCode((context, token) =>
             {
                 return CollectCommandHandler.Invoke(
                     token,
@@ -81,7 +80,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                 ShowSourcesOption
             };
 
-            showCommand.SetHandler(context =>
+            showCommand.SetAction(context =>
             {
                 ConfigShowCommandHandler.Invoke(
                     context.ParseResult.GetValue(UrlsOption),
@@ -94,7 +93,6 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                     context.ParseResult.GetValue(ConfigurationFilePathOption),
                     context.ParseResult.GetValue(ConfigLevelOption),
                     context.ParseResult.GetValue(ShowSourcesOption));
-                return 0;
             });
 
             Command configCommand = new Command(
