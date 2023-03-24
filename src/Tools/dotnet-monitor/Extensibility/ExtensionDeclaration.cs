83e4ebabf8ad25725fd2a8ef2f4254aaf4f32c43
Justin Anderson <jander-msft@users.noreply.github.com>
2023-03-23 21:37:30 -0700

    Simplified extension manifest (#4036)

--- src/Tools/dotnet-monitor/Extensibility/ExtensionDeclaration.cs
+++ src/Tools/dotnet-monitor/Extensibility/ExtensionDeclaration.cs
@@ -1,42 +1,57 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
-using System;
+using System.Globalization;
 
 namespace Microsoft.Diagnostics.Tools.Monitor.Extensibility
 {
     internal class ExtensionDeclaration
     {
         /// <summary>
-        /// Id of the extension. This should be namespace qualified like nuget packages.
-        /// </summary>
-        public string Id { get; set; }
-
-        /// <summary>
-        /// The version of the extension. This should be SemVer 2.0.
-        /// </summary>
-        public Version Version { get; set; }
-
-        /// <summary>
-        /// This is the relative path to the executable file to be launched.
-        /// </summary>
-        public string Program { get; set; }
-
-        /// <summary>
         /// This is the name that users specify in configuration to refer to the extension.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// An array of strings declaring what types of extensions are supported by this extension.
-        /// This should contain values from <see cref="ExtensionTypes"/>.
+        /// If specified, the executable file (without extension) to be launched when executing the extension.
         /// </summary>
-        public string[] SupportedExtensionTypes { get; set; }
+        /// <remarks>
+        /// Either <see cref="ExecutableFileName"/> or <see cref="AssemblyFileName"/> must be specified.
+        /// </remarks>
+        public string ExecutableFileName { get; set; }
 
         /// <summary>
-        /// Instructs dotnet-monitor to launch the extension using the shared .NET host (e.g. dotnet.exe).
+        /// If specified, executes the extension using the shared .NET host (e.g. dotnet.exe) with the specified entry point assembly (without extension). 
         /// </summary>
-        public bool UseSharedDotNetHost { get; set; }
+        /// <remarks>
+        /// Either <see cref="ExecutableFileName"/> or <see cref="AssemblyFileName"/> must be specified.
+        /// </remarks>
+        public string AssemblyFileName { get; set; }
+
+        public void Validate()
+        {
+            bool hasAssemblyFileName = !string.IsNullOrEmpty(AssemblyFileName);
+            bool hasExecutableFileName = !string.IsNullOrEmpty(ExecutableFileName);
+
+            if (hasAssemblyFileName && hasExecutableFileName)
+            {
+                ExtensionException.ThrowInvalidManifest(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        Strings.ErrorMessage_TwoFieldsCannotBeSpecified,
+                        nameof(AssemblyFileName),
+                        nameof(ExecutableFileName)));
+            }
+
+            if (!hasAssemblyFileName && !hasExecutableFileName)
+            {
+                ExtensionException.ThrowInvalidManifest(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        Strings.ErrorMessage_TwoFieldsMissing,
+                        nameof(AssemblyFileName),
+                        nameof(ExecutableFileName)));
+            }
+        }
     }
 }
