d370ef28727ae77dfa5a294c24cda9b294431fda
Joe Schmitt <joschmit@microsoft.com>
2023-05-17 11:24:07 -0700

    Cleanup

--- src/Tools/dotnet-monitor/StartupHook/StartupHookIdentifiers.cs
+++ src/Tools/dotnet-monitor/StartupHook/StartupHookIdentifiers.cs
@@ -13,16 +13,8 @@ namespace Microsoft.Diagnostics.Tools.Monitor.StartupHook
         {
             private const string StartupHookPrefix = ToolIdentifiers.StandardPrefix + "StartupHook_";
 
-            // This environment variable is automatically applied to a target process by the tool to inform
-            // the profiler which directory it should use to share files and information with dotnet-monitor.
-            public const string SharedPath = StartupHookPrefix + nameof(SharedPath);
-
-            // This environment variable name is embedded into the profiler and set at profiler initialization.
-            // The value is determined BEFORE native build by the generation of the product version into the
-            // _productversion.h header file.
+            // The full path of a HostingStartup assembly to try and load into a target process.
             public const string HostingStartupPath = StartupHookPrefix + nameof(HostingStartupPath);
-
-
         }
     }
 }
