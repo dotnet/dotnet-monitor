22fece2e0880853112dee27f6821c1d9241879a4
Joe Schmitt <joschmit@microsoft.com>
2023-05-18 13:22:11 -0700

    Min diff

--- src/Tools/dotnet-monitor/StartupHook/StartupHookIdentifiers.cs
+++ src/Tools/dotnet-monitor/StartupHook/StartupHookIdentifiers.cs
@@ -1,11 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if UNITTEST
-namespace Microsoft.Diagnostics.Monitoring.TestCommon
-#else
 namespace Microsoft.Diagnostics.Tools.Monitor.StartupHook
-#endif
 {
     public static class StartupHookIdentifiers
     {
