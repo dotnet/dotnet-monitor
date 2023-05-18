841e20552d63e73384a8ca7f8ca80ea421f8f4b1
Joe Schmitt <joschmit@microsoft.com>
2023-05-18 13:24:19 -0700

    nit: lint

--- src/Microsoft.Diagnostics.Monitoring.StartupHook/AspNetHostingStartupLoader.cs
+++ src/Microsoft.Diagnostics.Monitoring.StartupHook/AspNetHostingStartupLoader.cs
@@ -12,31 +12,31 @@ namespace Microsoft.Diagnostics.Monitoring.StartupHook
     {
         private const string HostingStartupEnvVariable = "ASPNETCORE_HOSTINGSTARTUPASSEMBLIES";
 
-        private readonly string FilePath;
-        private readonly string ShortAssemblyName;
+        private readonly string _filePath;
+        private readonly string _simpleAssemblyName;
 
         private long _disposedState;
 
         public AspNetHostingStartupLoader(string filePath)
         {
-            FilePath = filePath;
-            ShortAssemblyName = CalculateAssemblyName(filePath);
+            _filePath = filePath;
+            _simpleAssemblyName = CalculateSimpleAssemblyName(filePath);
 
-            AppendToEnvironmentVariable(HostingStartupEnvVariable, ShortAssemblyName);
+            AppendToEnvironmentVariable(HostingStartupEnvVariable, _simpleAssemblyName);
             AssemblyLoadContext.Default.Resolving += AssemblyResolver;
         }
 
         private Assembly? AssemblyResolver(AssemblyLoadContext context, AssemblyName assemblyName)
         {
-            if (ShortAssemblyName.Equals(assemblyName.Name, StringComparison.OrdinalIgnoreCase))
+            if (_simpleAssemblyName.Equals(assemblyName.Name, StringComparison.OrdinalIgnoreCase))
             {
-                return AssemblyLoadContext.Default.LoadFromAssemblyPath(FilePath);
+                return AssemblyLoadContext.Default.LoadFromAssemblyPath(_filePath);
             }
 
             return null;
         }
 
-        private static string CalculateAssemblyName(string filePath)
+        private static string CalculateSimpleAssemblyName(string filePath)
         {
             string fileName = Path.GetFileName(filePath);
             const string dllExtension = ".dll";
