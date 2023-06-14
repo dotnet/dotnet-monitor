fa364dd537cf88dabb8c2d33da86f97bc6ca66f8
Justin Anderson <jander@microsoft.com>
2023-06-14 14:14:36 -0700

    Simplify demultiplexer

--- src/Microsoft.Diagnostics.Monitoring.StartupHook/Exceptions/Pipeline/Steps/ExceptionDemultiplexerPipelineStep.cs
+++ src/Microsoft.Diagnostics.Monitoring.StartupHook/Exceptions/Pipeline/Steps/ExceptionDemultiplexerPipelineStep.cs
@@ -2,9 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 
 namespace Microsoft.Diagnostics.Monitoring.StartupHook.Exceptions.Pipeline.Steps
 {
@@ -28,28 +25,18 @@ namespace Microsoft.Diagnostics.Monitoring.StartupHook.Exceptions.Pipeline.Steps
         {
             ExceptionPipelineExceptionContext innerContext = new(context.Timestamp, isInnerException: true);
 
-            if (null != exception.InnerException)
-            {
-                Invoke(exception.InnerException, innerContext);
-            }
-
+            // AggregateException will always pull the first exception out of the list of inner exceptions
+            // and use that as its InnerException property. No need to report the InnerException property value.
             if (exception is AggregateException aggregateException)
             {
-                int startingIndex = 0;
-                // Skip the first exception since it was already reported for the InnerException property
-                // AggregateException will always pull the first exception out of the list of inner exceptions
-                // and use that as its InnerException property.
-                if (null != exception.InnerException)
-                {
-                    Debug.Assert(aggregateException.InnerExceptions.Count == 0 || aggregateException.InnerExceptions[0] == exception.InnerException);
-
-                    startingIndex = 1;
-                }
-
-                for (int i = startingIndex; i < aggregateException.InnerExceptions.Count; i++)
+                for (int i = 0; i < aggregateException.InnerExceptions.Count; i++)
                 {
                     Invoke(aggregateException.InnerExceptions[i], innerContext);
                 }
+            }
+            else if (null != exception.InnerException)
+            {
+                Invoke(exception.InnerException, innerContext);
             }
 
             _next(exception, context);
