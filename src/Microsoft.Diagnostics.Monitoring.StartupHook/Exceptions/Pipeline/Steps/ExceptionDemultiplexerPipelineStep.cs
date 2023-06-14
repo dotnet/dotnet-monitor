9b6fc8259045e7cf2d68b439fc09339722d1247e
Justin Anderson <jander-msft@users.noreply.github.com>
2023-06-14 15:15:15 -0700

    Capture inner exceptions in startup hook (#4649)

---
+++ src/Microsoft.Diagnostics.Monitoring.StartupHook/Exceptions/Pipeline/Steps/ExceptionDemultiplexerPipelineStep.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.Diagnostics.Monitoring.StartupHook.Exceptions.Pipeline.Steps
+{
+    /// <summary>
+    /// An exception pipeline step that reports the related exception instances of
+    /// the provided exception, namely the inner exception instances, as well as the main
+    /// exception itself.
+    /// </summary>
+    internal class ExceptionDemultiplexerPipelineStep
+    {
+        private readonly ExceptionPipelineDelegate _next;
+
+        public ExceptionDemultiplexerPipelineStep(ExceptionPipelineDelegate next)
+        {
+            ArgumentNullException.ThrowIfNull(next);
+
+            _next = next;
+        }
+
+        public void Invoke(Exception exception, ExceptionPipelineExceptionContext context)
+        {
+            ExceptionPipelineExceptionContext innerContext = new(context.Timestamp, isInnerException: true);
+
+            // AggregateException will always pull the first exception out of the list of inner exceptions
+            // and use that as its InnerException property. No need to report the InnerException property value.
+            if (exception is AggregateException aggregateException)
+            {
+                for (int i = 0; i < aggregateException.InnerExceptions.Count; i++)
+                {
+                    Invoke(aggregateException.InnerExceptions[i], innerContext);
+                }
+            }
+            else if (null != exception.InnerException)
+            {
+                Invoke(exception.InnerException, innerContext);
+            }
+
+            _next(exception, context);
+        }
+    }
+}
