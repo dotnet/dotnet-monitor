99ffa859e9f1142edd11d58b97d31e91fe54fc42
Joe Schmitt <joschmit@microsoft.com>
2023-07-06 14:10:00 -0700

    Min diff

--- src/Tools/dotnet-monitor/Profiler/ProfilerEndpointInfoSourceCallbacks.cs
+++ src/Tools/dotnet-monitor/Profiler/ProfilerEndpointInfoSourceCallbacks.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Diagnostics.Monitoring.WebApi;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
