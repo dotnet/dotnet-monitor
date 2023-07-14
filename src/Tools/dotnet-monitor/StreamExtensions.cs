347a4707098c55300031f37866a761a067297b58
Justin Anderson <jander@microsoft.com>
2023-07-14 15:00:53 -0700

    Sort usings

--- src/Tools/dotnet-monitor/StreamExtensions.cs
+++ src/Tools/dotnet-monitor/StreamExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Threading.Tasks;
-using System.Threading;
 using System;
-using System.IO;
 using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Diagnostics.Tools.Monitor
 {
