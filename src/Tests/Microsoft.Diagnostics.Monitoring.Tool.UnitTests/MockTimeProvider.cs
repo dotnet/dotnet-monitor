40722da014c3ac520ab49fbb62345e2ad76c6432
Justin Anderson <jander@microsoft.com>
2023-07-14 17:24:09 -0700

    Modify C# file

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/MockTimeProvider.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.UnitTests/MockTimeProvider.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Test!
+
 using System;
 #if !NET8_0_OR_GREATER
 using Microsoft.Diagnostics.Tools.Monitor;
