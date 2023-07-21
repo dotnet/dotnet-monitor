396e32db9829b9e682a7899ffbcb6b3070d4598b
Justin Anderson <jander@microsoft.com>
2023-07-21 11:45:42 -0700

    Use function on non-Windows

--- eng/helix/payload/setupnode.sh
+++ eng/helix/payload/setupnode.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/$1/bin:$PATH
-npm config set prefix $HELIX_WORKITEM_ROOT/.npm
-export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
+function setupNode {
+  export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/$1/bin:$PATH
+  npm config set prefix $HELIX_WORKITEM_ROOT/.npm
+  export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
+}
