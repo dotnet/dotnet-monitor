2853458501ea09893120067b647dafed66e6be60
Justin Anderson <jander@microsoft.com>
2023-07-21 12:53:39 -0700

    Remove function keyword

--- eng/helix/payload/setupnode.sh
+++ eng/helix/payload/setupnode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function setupNode {
+setupNode() {
   export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/$1/bin:$PATH
   npm config set prefix $HELIX_WORKITEM_ROOT/.npm
   export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
