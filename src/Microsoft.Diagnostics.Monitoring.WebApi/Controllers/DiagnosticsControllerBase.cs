fd0f2f419496ab00ee19ad463d936006e8df0369
Justin Anderson <jander@microsoft.com>
2023-07-21 13:24:13 -0700

    Move InvokeForProcess support into base controller class

---
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/Controllers/DiagnosticsControllerBase.cs
@@ -0,0 +1,74 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.Monitoring.WebApi.Controllers
+{
+    public abstract class DiagnosticsControllerBase : ControllerBase
+    {
+        internal DiagnosticsControllerBase(IDiagnosticServices diagnosticServices, ILogger logger)
+        {
+            DiagnosticServices = diagnosticServices ?? throw new ArgumentNullException(nameof(diagnosticServices));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        internal Task<ActionResult> InvokeForProcess(Func<IProcessInfo, ActionResult> func, ProcessKey? processKey, string artifactType = null)
+        {
+            Func<IProcessInfo, Task<ActionResult>> asyncFunc =
+                processInfo => Task.FromResult(func(processInfo));
+
+            return InvokeForProcess(asyncFunc, processKey, artifactType);
+        }
+
+        internal async Task<ActionResult> InvokeForProcess(Func<IProcessInfo, Task<ActionResult>> func, ProcessKey? processKey, string artifactType)
+        {
+            ActionResult<object> result = await InvokeForProcess<object>(async processInfo => await func(processInfo), processKey, artifactType);
+
+            return result.Result;
+        }
+
+        internal Task<ActionResult<T>> InvokeForProcess<T>(Func<IProcessInfo, ActionResult<T>> func, ProcessKey? processKey, string artifactType = null)
+        {
+            return InvokeForProcess(processInfo => Task.FromResult(func(processInfo)), processKey, artifactType);
+        }
+
+        internal async Task<ActionResult<T>> InvokeForProcess<T>(Func<IProcessInfo, Task<ActionResult<T>>> func, ProcessKey? processKey, string artifactType = null)
+        {
+            IDisposable artifactTypeRegistration = null;
+            if (!string.IsNullOrEmpty(artifactType))
+            {
+                KeyValueLogScope artifactTypeScope = new KeyValueLogScope();
+                artifactTypeScope.AddArtifactType(artifactType);
+                artifactTypeRegistration = Logger.BeginScope(artifactTypeScope);
+            }
+
+            try
+            {
+                return await this.InvokeService(async () =>
+                {
+                    IProcessInfo processInfo = await DiagnosticServices.GetProcessAsync(processKey, HttpContext.RequestAborted);
+
+                    KeyValueLogScope processInfoScope = new KeyValueLogScope();
+                    processInfoScope.AddArtifactEndpointInfo(processInfo.EndpointInfo);
+                    using var _ = Logger.BeginScope(processInfoScope);
+
+                    Logger.ResolvedTargetProcess();
+
+                    return await func(processInfo);
+                }, Logger);
+            }
+            finally
+            {
+                artifactTypeRegistration?.Dispose();
+            }
+        }
+
+        internal IDiagnosticServices DiagnosticServices { get; }
+
+        protected ILogger Logger { get; }
+    }
+}
