a9502146b42e3d5d692c59f1ea557c30b04a0ff4
Justin Anderson <jander-msft@users.noreply.github.com>
2023-07-24 11:27:20 -0700

    Install NodeJs and Azurite on Helix (#4935)

---
+++ eng/helix/payload/installazurite.cmd
@@ -0,0 +1,3 @@
+npm install -g azurite
+if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
+set TEST_AZURITE_MUST_INITIALIZE=1
---
+++ eng/helix/payload/setupnode.cmd
@@ -0,0 +1,4 @@
+set PATH=%HELIX_CORRELATION_PAYLOAD%\nodejs\%1;%PATH%
+npm config set prefix %HELIX_WORKITEM_ROOT%\.npm
+if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
+set PATH=%HELIX_WORKITEM_ROOT%\.npm;%PATH%
