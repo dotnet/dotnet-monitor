cfdf28dfd6e75cb92aa10013d3f9fa457018b44e
Justin Anderson <jander@microsoft.com>
2023-07-24 09:14:25 -0700

    Terminate scripts on error

--- eng/helix/payload/installazurite.sh
+++ eng/helix/payload/installazurite.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-npm install -g azurite
+npm install -g azurite || exit 1
 export TEST_AZURITE_MUST_INITIALIZE=1
--- eng/helix/payload/setupnode.sh
+++ eng/helix/payload/setupnode.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 setupNode() {
   export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/$1/bin:$PATH
-  npm config set prefix $HELIX_WORKITEM_ROOT/.npm
+  npm config set prefix $HELIX_WORKITEM_ROOT/.npm || exit 1
   export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
 }
