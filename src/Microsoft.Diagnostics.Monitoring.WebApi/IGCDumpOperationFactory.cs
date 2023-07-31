b4a1ed9fc2a237fbd09fb4d1aad26b2974afb25d
Justin Anderson <jander-msft@users.noreply.github.com>
2023-07-31 11:05:22 -0700

    Convert gcdump to artifact operations (#5019)

---
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/IGCDumpOperationFactory.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Diagnostics.Monitoring.WebApi
+{
+    internal interface IGCDumpOperationFactory
+    {
+        IArtifactOperation Create(IEndpointInfo endpointInfo);
+    }
+}
