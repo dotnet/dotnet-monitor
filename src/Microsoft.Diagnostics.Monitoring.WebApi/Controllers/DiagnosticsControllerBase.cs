429fabcb065e406fb89ff6f636da86827c8790b4
Justin Anderson <jander@microsoft.com>
2023-08-02 16:03:28 -0700

    Allow exceptions requests to be egressed and tagged

--- src/Microsoft.Diagnostics.Monitoring.WebApi/Controllers/DiagnosticsControllerBase.cs
+++ src/Microsoft.Diagnostics.Monitoring.WebApi/Controllers/DiagnosticsControllerBase.cs
@@ -10,9 +10,10 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi.Controllers
 {
     public abstract class DiagnosticsControllerBase : ControllerBase
     {
-        private protected DiagnosticsControllerBase(IDiagnosticServices diagnosticServices, ILogger logger)
+        private protected DiagnosticsControllerBase(IDiagnosticServices diagnosticServices, EgressOperationStore operationStore, ILogger logger)
         {
             DiagnosticServices = diagnosticServices ?? throw new ArgumentNullException(nameof(diagnosticServices));
+            OperationStore = operationStore ?? throw new ArgumentNullException(nameof(operationStore));
             Logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -67,7 +68,63 @@ namespace Microsoft.Diagnostics.Monitoring.WebApi.Controllers
             }
         }
 
+        private protected async Task<ActionResult> Result(
+            string artifactType,
+            string providerName,
+            IArtifactOperation operation,
+            IProcessInfo processInfo,
+            string tags,
+            bool asAttachment = true)
+        {
+            KeyValueLogScope scope = Utilities.CreateArtifactScope(artifactType, processInfo.EndpointInfo);
+
+            if (string.IsNullOrEmpty(providerName))
+            {
+                await RegisterCurrentHttpResponseAsOperation(processInfo, artifactType, tags, operation);
+                return new OutputStreamResult(
+                    operation,
+                    asAttachment ? operation.GenerateFileName() : null,
+                    scope);
+            }
+            else
+            {
+                return await SendToEgress(new EgressOperation(
+                    operation,
+                    providerName,
+                    processInfo,
+                    scope,
+                    tags),
+                    limitKey: artifactType);
+            }
+        }
+
+        private async Task RegisterCurrentHttpResponseAsOperation(IProcessInfo processInfo, string artifactType, string tags, IArtifactOperation operation = null)
+        {
+            // While not strictly a Location redirect, use the same header as externally egressed operations for consistency.
+            HttpContext.Response.Headers["Location"] = await RegisterOperation(
+                new HttpResponseEgressOperation(HttpContext, processInfo, tags, operation),
+                limitKey: artifactType);
+        }
+
+        private async Task<string> RegisterOperation(IEgressOperation egressOperation, string limitKey)
+        {
+            // Will throw TooManyRequestsException if there are too many concurrent operations.
+            Guid operationId = await OperationStore.AddOperation(egressOperation, limitKey);
+            return this.Url.Action(
+                action: nameof(OperationsController.GetOperationStatus),
+                controller: OperationsController.ControllerName, new { operationId = operationId },
+                protocol: this.HttpContext.Request.Scheme, this.HttpContext.Request.Host.ToString());
+        }
+
+        private async Task<ActionResult> SendToEgress(IEgressOperation egressOperation, string limitKey)
+        {
+            string operationUrl = await RegisterOperation(egressOperation, limitKey);
+            return Accepted(operationUrl);
+        }
+
         private protected IDiagnosticServices DiagnosticServices { get; }
+
+        private protected EgressOperationStore OperationStore { get; }
 
         protected ILogger Logger { get; }
     }
