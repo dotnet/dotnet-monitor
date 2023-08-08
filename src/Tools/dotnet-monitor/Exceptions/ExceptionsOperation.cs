4ffa7053c441cc0a8cc4aa9a6ec227f55d29cf01
kkeirstead <kkeirstead@microsoft.com>
2023-08-08 10:17:30 -0700

    Removed unused using.

--- src/Tools/dotnet-monitor/Exceptions/ExceptionsOperation.cs
+++ src/Tools/dotnet-monitor/Exceptions/ExceptionsOperation.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
