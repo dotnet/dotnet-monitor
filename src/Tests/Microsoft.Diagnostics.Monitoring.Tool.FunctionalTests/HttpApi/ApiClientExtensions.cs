fcddc08e530025bff8d74c79930fd4a70862b3d8
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2023-08-24 11:51:21 -0700

    [Parameter Capturing] Add functional API tests (#5178)

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/HttpApi/ApiClientExtensions.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/HttpApi/ApiClientExtensions.cs
@@ -494,11 +494,27 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests.HttpApi
             return operationResult;
         }
 
+        /// <summary>
+        /// POST /parameters
+        /// </summary>
+        public static Task<OperationResponse> CaptureParametersAsync(this ApiClient client, int pid, TimeSpan duration, CaptureParametersConfiguration config)
+        {
+            return client.CaptureParametersAsync(pid, duration, config, TestTimeouts.HttpApi);
+        }
+
+        /// <summary>
+        /// POST /parameters
+        /// </summary>
+        public static async Task<OperationResponse> CaptureParametersAsync(this ApiClient client, int pid, TimeSpan duration, CaptureParametersConfiguration config, TimeSpan timeout)
+        {
+            using CancellationTokenSource timeoutSource = new(timeout);
+            return await client.CaptureParametersAsync(pid, duration, config, timeoutSource.Token).ConfigureAwait(false);
+        }
+
         public static Task<OperationStatusResponse> WaitForOperationToStart(this ApiClient apiClient, Uri operationUrl)
         {
             return apiClient.WaitForOperationToStart(operationUrl, TestTimeouts.OperationTimeout);
         }
-
 
         public static async Task<OperationStatusResponse> WaitForOperationToStart(this ApiClient apiClient, Uri operationUrl, TimeSpan timeout)
         {
