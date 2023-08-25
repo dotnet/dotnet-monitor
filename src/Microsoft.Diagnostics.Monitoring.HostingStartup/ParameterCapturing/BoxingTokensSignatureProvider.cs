203a53a49bf03d0183d50684338f8d448230e2c8
Joe Schmitt <joschmit@microsoft.com>
2023-08-25 13:43:51 -0700

    Run linter

--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/BoxingTokensSignatureProvider.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/BoxingTokensSignatureProvider.cs
@@ -1,11 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Immutable;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
-using static Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing.BoxingTokens;
 
 namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing
 {
