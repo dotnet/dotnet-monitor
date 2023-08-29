3a5929c82f59447a1a00d9f71bd23dec83dae799
Justin Anderson <jander-msft@users.noreply.github.com>
2023-08-29 15:01:17 -0700

    Change ExceptionsStore to allow multiple callbacks (#5213)

--- src/Tools/dotnet-monitor/Exceptions/ExceptionsStoreCallbackBase.cs
+++ src/Tools/dotnet-monitor/Exceptions/ExceptionsStoreCallbackBase.cs
@@ -5,9 +5,12 @@ using Microsoft.Diagnostics.Monitoring.WebApi.Exceptions;
 
 namespace Microsoft.Diagnostics.Tools.Monitor.Exceptions
 {
-    internal abstract class ExceptionsStoreCallback
+    internal abstract class ExceptionsStoreCallbackBase :
+        IExceptionsStoreCallback
     {
         public virtual void AfterAdd(IExceptionInstance instance) { }
+
+        public virtual void AfterRemove(IExceptionInstance instance) { }
 
         public virtual void BeforeAdd(IExceptionInstance instance) { }
     }
