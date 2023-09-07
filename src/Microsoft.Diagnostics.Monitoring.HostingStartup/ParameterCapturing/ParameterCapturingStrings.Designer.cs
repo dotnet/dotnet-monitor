7b19b961e412a11bf358efc80f604694e7d7991b
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2023-09-07 16:27:58 -0700

    [Parameter Capturing] Log when probe installation is cancelled (#5290)

--- src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/ParameterCapturingStrings.Designer.cs
+++ src/Microsoft.Diagnostics.Monitoring.HostingStartup/ParameterCapturing/ParameterCapturingStrings.Designer.cs
@@ -62,6 +62,15 @@ namespace Microsoft.Diagnostics.Monitoring.HostingStartup.ParameterCapturing {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Cancellation has been requested during probe installation. Cancellation tokens request state (Provided:{isProvidedTokenCancelled}, Disposal:{isDisposalTokenCancelled})..
+        /// </summary>
+        internal static string CancellationRequestedDuringProbeInstallation {
+            get {
+                return ResourceManager.GetString("CancellationRequestedDuringProbeInstallation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The following method descriptions are not allowed: {0}.
         /// </summary>
         internal static string DeniedMethodsFormatString {
