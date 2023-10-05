2ab0d24b7f5fdb126caed2d49b28b4482a94f9fa
Joe Schmitt <joschmit@microsoft.com>
2023-10-05 15:49:32 -0700

    Cache nodejs

---
+++ eng/helix/GetNodejsVersion.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding(PositionalBinding=$false)]
+Param(
+    [ValidateSet("x86","x64","arm64")][string][Alias('a', "platform")]$architecture = [System.Runtime.InteropServices.RuntimeInformation]::ProcessArchitecture.ToString().ToLowerInvariant(),
+    [int] $MajorVersion,
+    [string] $Mirror = "https://nodejs.org/dist",
+    [Parameter(Mandatory=$false)][string] $TaskVariableName = $null
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference="Stop"
+
+$availableVersion = Invoke-WebRequest "$Mirror/index.json" -MaximumRetryCount 5 | ConvertFrom-Json
+$latestMatchingVersion=$null
+foreach ($verObj in $availableVersion) {
+    if ($verObj.version.StartsWith("v$MajorVersion.")) {
+        $latestMatchingVersion=$verObj.version
+        break
+    }
+}
+
+if ($latestMatchingVersion -eq $null) {
+    Write-Error "Could not find matching version"
+    exit 1;
+}
+
+if ($TaskVariableName) {
+    & $PSScriptRoot\release\Scripts\SetTaskVariable.ps1 `
+        -Name $TaskVariableName `
+        -Value $latestMatchingVersion
+}
+
+Write-Output $latestMatchingVersion
---
+++ eng/helix/InstallNodejs.ps1
@@ -0,0 +1,50 @@
+[CmdletBinding(PositionalBinding=$false)]
+Param(
+    [ValidateSet("x86","x64","arm64")][string]$Architecture,
+    [string] $Version,
+    [string] $Mirror = "https://nodejs.org/dist",
+    [string] $DestinationFolder
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference="Stop"
+
+if ($IsMacOS) {
+    $nodePlatform="darwin"
+    $nodeExt="tar.gz"
+} elseif ($IsLinux) {
+    $nodePlatform="linux"
+    $nodeExt="tar.gz"
+} elseif ($IsWindows) {
+    $nodePlatform="win"
+    $nodeExt="zip"
+} else {
+    Write-Error "Unknown OS"
+    exit 1;
+}
+
+if (Test-Path -Path $DestinationFolder) {
+    Write-Output "$DestinationFolder already exists, skipping"
+    exit 0
+}
+
+$nodeName="node-$Version-$nodePlatform-$Architecture"
+$nodeUrl="$Mirror/$Version/$nodeName.$nodeExt"
+Write-Verbose "URL: $nodeUrl"
+
+$tempFolderPath = Join-Path $Env:Temp $(New-Guid)
+New-Item -ItemType Directory -Force -Path $tempFolderPath
+
+$archiveFile= Join-Path $tempFolderPath "$nodeName.$nodeExt"
+Write-Verbose "Archive file: $archiveFile"
+
+Invoke-WebRequest $nodeUrl -OutFile $archiveFile -MaximumRetryCount 5
+if ($IsWindows) {
+    Expand-Archive $archiveFile -DestinationPath $tempFolderPath
+    Move-Item -Path $(Join-Path $tempFolderPath $nodeName) -Destination $DestinationFolder
+} else {
+    New-Item -ItemType Directory -Force -Path $DestinationFolder
+    & tar --strip-components 1 -xzf "$archiveFile" --no-same-owner --directory "$DestinationFolder"
+}
+
+Remove-Item -Recurse -Force $tempFolderPath
