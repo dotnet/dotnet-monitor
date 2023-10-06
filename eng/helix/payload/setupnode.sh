88f092ac6808d45f602d993135ec2faeaf8ec24c
Joe Schmitt <joschmit@microsoft.com>
2023-10-06 14:09:34 -0700

    Fix symlinks

--- eng/helix/payload/setupnode.sh
+++ eng/helix/payload/setupnode.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-setupNode() {
-  export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/$1/bin:$PATH
-  npm config set prefix $HELIX_WORKITEM_ROOT/.npm || exit 1
-  export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
-}
+export PATH=$HELIX_CORRELATION_PAYLOAD/nodejs/bin:$PATH
+npm config set prefix $HELIX_WORKITEM_ROOT/.npm || exit 1
+export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
