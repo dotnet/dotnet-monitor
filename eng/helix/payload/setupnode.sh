838da4db9bb1deace3d46f85a1440171ad50a9de
Joe Schmitt <joschmit@microsoft.com>
2023-10-09 15:29:19 -0700

    Alias for *nix support

--- eng/helix/payload/setupnode.sh
+++ eng/helix/payload/setupnode.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
+
 #
-# Recreate executable symlinks.
-# When helix transfers a correlation payload it will zip the contents for transit, resulting in symbolic links being dereferenced.
+# Create an npm alias.
+#
+# When helix transfers a correlation payload it will zip the contents for transit, resulting in symbolic links to be dereferenced.
 # In the case of common node tools (e.g. npm) this is not ideal as the linked scripts contain relative file paths which will be broken.
 #
-# Ideally we could simply transfer a tar file instead and unpack it here, however not all helix containers have the necessary tooling
-# to process tar files nor do all containers have ability to change the EUID, meaning the distro's package manager cannot be used to
-# install the missing tools.
+# Ideally we could simply transfer a tar file instead and unpack it here, however not all helix containers
+# have the necessary tooling to process tar files, let alone the ability to change the EUID to use
+# the distro's package manager to install them.
+#
+# Alternatively we could recreate the symlinks. However certain helix containers have read-only file systems which makes this impossible.
+# Creating a custom alias that directly invokes node with the npm-cli script will work across all of these constraints on all *nix platforms.
 #
 NODE_BIN=$HELIX_CORRELATION_PAYLOAD/nodejs/bin
-rm $NODE_BIN/corepack || exit 1
-ln -s ../lib/node_modules/corepack/dist/corepack.js $NODE_BIN/corepack || exit 1
-
-rm $NODE_BIN/npm || exit 1
-ln -s ../lib/node_modules/npm/bin/npm-cli.js $NODE_BIN/npm || exit 1
-
-rm $NODE_BIN/npx || exit 1
-ln -s ../lib/node_modules/npm/bin/npx-cli.js $NODE_BIN/npx || exit 1
+alias helix_npm="node $HELIX_CORRELATION_PAYLOAD/nodejs/lib/node_modules/npm/bin/npm-cli.js"
 
 export PATH=$NODE_BIN:$PATH
-npm config set prefix $HELIX_WORKITEM_ROOT/.npm || exit 1
+helix_npm config set prefix $HELIX_WORKITEM_ROOT/.npm || exit 1
 export PATH=$HELIX_WORKITEM_ROOT/.npm/bin:$PATH
