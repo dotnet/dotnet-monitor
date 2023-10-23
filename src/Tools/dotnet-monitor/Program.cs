b0d134091e7d730eef6c3085af3d51bf1a61eb37
Justin Anderson <jander@microsoft.com>
2023-10-23 15:10:11 -0700

    Some C# change

--- src/Tools/dotnet-monitor/Program.cs
+++ src/Tools/dotnet-monitor/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Some kind of change
+
 using Microsoft.Diagnostics.Tools.Monitor.Commands;
 using System;
 using System.CommandLine;
