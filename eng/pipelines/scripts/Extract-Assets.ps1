36c9b87bd9c74c3d07f626c5568e28c2ef0f6584
Justin Anderson <jander-msft@users.noreply.github.com>
2023-11-17 15:44:23 -0800

    Add compliance pipeline and scripts (#5707)

---
+++ eng/pipelines/scripts/Extract-Assets.ps1
@@ -0,0 +1,66 @@
+param(
+  [Parameter(Mandatory=$true)][string] $SourcePath,
+  [Parameter(Mandatory=$true)][string] $TargetPath,
+  [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 2.0
+
+if ($null -ne $properties) {
+  Write-Host "Unexpected extra parameters: $properties."
+  exit 1
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function Create-PackageDir() {
+  param( 
+    [string] $FileName
+  )
+  $extractionPath = Join-Path -Path $TargetPath -ChildPath ($FileName + "_dir")
+  New-Item -ItemType Directory -Force -Path $extractionPath | Out-Null
+  return $extractionPath
+}
+
+function Extract-Zip() {
+  param(
+    [System.IO.FileInfo] $FileInfo
+  )
+  $extractionPath = Create-PackageDir -FileName $FileInfo.Name
+  Write-Host "Extracting $($FileInfo.Name)"
+  [System.IO.Compression.ZipFile]::ExtractToDirectory($FileInfo.FullName, $extractionPath)
+}
+
+function Extract-TarGz() {
+  param(
+    [System.IO.FileInfo] $FileInfo
+  )
+  $extractionPath = Create-PackageDir -FileName $FileInfo.Name
+  Write-Host "Extracting $($FileInfo.Name)"
+  tar -xzf $FileInfo.FullName -C $extractionPath
+}
+
+function Copy-File() {
+  param(
+    [System.IO.FileInfo] $FileInfo
+  )
+  Write-Host "Copying $($FileInfo.Name)"
+  Copy-Item -Path $FileInfo.FullName -Destination $TargetPath
+}
+
+New-Item -ItemType Directory -Force -Path $TargetPath | Out-Null
+
+foreach ($fileInfo in (Get-ChildItem $SourcePath -Recurse -Attributes !Directory)) {
+  if ($fileInfo.Name.EndsWith('symbols.nupkg')) {
+    Extract-Zip -FileInfo $fileInfo
+  } elseif ($fileInfo.Name.EndsWith('nupkg')) {
+    Extract-Zip -FileInfo $fileInfo
+  } elseif ($fileInfo.Name.EndsWith('zip')) {
+    Extract-Zip -FileInfo $fileInfo
+  } elseif ($fileInfo.Name.EndsWith('tar.gz')) {
+    Extract-TarGz -FileInfo $fileInfo
+  } else {
+    Copy-File -FileInfo $fileInfo
+  }
+}
