c6baf461a93b949683c67eec5c251713266f8a65
Justin Anderson <jander-msft@users.noreply.github.com>
2023-12-18 16:56:55 -0800

    Lower CPU threshold in collection rule integration tests (#5768)

--- src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/CollectionRuleTests.cs
+++ src/Tests/Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests/CollectionRuleTests.cs
@@ -109,11 +109,11 @@ namespace Microsoft.Diagnostics.Monitoring.Tool.FunctionalTests
                     runner.ConfigurationFromEnvironment.CreateCollectionRule(DefaultRuleName)
                         .SetEventCounterTrigger(options =>
                         {
-                            // cpu usage greater that 5% for 2 seconds
+                            // cpu usage greater than 1% for 1 second
                             options.ProviderName = "System.Runtime";
                             options.CounterName = "cpu-usage";
-                            options.GreaterThan = 5;
-                            options.SlidingWindowDuration = TimeSpan.FromSeconds(2);
+                            options.GreaterThan = 1;
+                            options.SlidingWindowDuration = TimeSpan.FromSeconds(1);
                         })
                         .AddExecuteActionAppAction(Assembly.GetExecutingAssembly(), "TextFileOutput", ExpectedFilePath, ExpectedFileContent)
                         .SetActionLimits(count: 1);
