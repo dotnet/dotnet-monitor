6e9e510dfd38b5a72190d0ef367c31c9141f5d43
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2024-01-23 08:14:09 -0800

    Update EgressHelper.cs

--- src/Microsoft.Diagnostics.Monitoring.Extension.Common/EgressHelper.cs
+++ src/Microsoft.Diagnostics.Monitoring.Extension.Common/EgressHelper.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Just adding some comments...
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
