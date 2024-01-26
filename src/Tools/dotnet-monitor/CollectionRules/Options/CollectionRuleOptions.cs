88f8a4d2022533f53fc77fa8142f02facc5e72d5
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2024-01-26 14:46:48 -0800

    Update CollectionRuleOptions.cs

--- src/Tools/dotnet-monitor/CollectionRules/Options/CollectionRuleOptions.cs
+++ src/Tools/dotnet-monitor/CollectionRules/Options/CollectionRuleOptions.cs
@@ -34,5 +34,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options
         public CollectionRuleLimitsOptions Limits { get; set; }
 
         internal List<ValidationResult> ErrorList { get; } = new List<ValidationResult>();
+
+        public int number = 5; // TESTING ONLY
     }
 }
