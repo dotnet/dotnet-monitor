2530d3a773be8e7e573aed69d10653e1e9ceb4cb
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2024-01-31 10:40:31 -0800

    Update ServiceCollectionExtensions.cs

--- src/Tools/dotnet-monitor/ServiceCollectionExtensions.cs
+++ src/Tools/dotnet-monitor/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Diagnostics.Monitoring.EventPipe.Triggers;
