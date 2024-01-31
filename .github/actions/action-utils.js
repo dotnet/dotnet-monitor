e51b987cbe545278ff5fb6213063b689be607237
kkeirstead <85592574+kkeirstead@users.noreply.github.com>
2024-01-31 14:39:39 -0800

    Update action-utils.js

--- .github/actions/action-utils.js
+++ .github/actions/action-utils.js
@@ -51,7 +51,7 @@ module.exports.friendlyDateFromISODate = function(isoDate) {
 
 module.exports.splitVersionTag = splitVersionTag;
 module.exports.readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
-module.exports.readFileSync = (fileName) => util.promisify(fs.readFileSync)(fileName, 'utf8');
+module.exports.readFileSync = (fileName) => fs.readFileSync(fileName, 'utf8');
 module.exports.writeFile = (fileName, contents) => util.promisify(fs.writeFile)(fileName, contents);
-module.exports.writeFileSync = (fileName, contents) => util.promisify(fs.writeFileSync)(fileName, contents);
+module.exports.writeFileSync = (fileName, contents) => fs.writeFileSync(fileName, contents);
 module.exports.readdir = (dirName, callback) => util.promisify(fs.readdir)(dirName, callback);
