6d17425d8d4839f77f8f529e1b1240775e13c509
Claudiu Guiman <clguiman@microsoft.com>
2024-05-15 14:20:09 -0400

    ProbeFaulted should throw InvalidOperationException

--- src/Tools/dotnet-monitor/ParameterCapturing/CaptureParametersOperation.cs
+++ src/Tools/dotnet-monitor/ParameterCapturing/CaptureParametersOperation.cs
@@ -219,7 +219,6 @@ namespace Microsoft.Diagnostics.Tools.Monitor.ParameterCapturing
             {
                 case ParameterCapturingEvents.CapturingFailedReason.UnresolvedMethods:
                 case ParameterCapturingEvents.CapturingFailedReason.InvalidRequest:
-                case ParameterCapturingEvents.CapturingFailedReason.ProbeFaulted:
                     ex = new MonitoringException(args.Details);
                     break;
                 default:
