e6697049ee814568e102f570c1e2c30d389739e2
Justin Anderson <jander-msft@users.noreply.github.com>
2024-05-16 11:12:05 -0700

    Use add_compiler_options to enable spectre mitigations and sdl checks (#6658)

--- eng/native/override/configurecompiler.cmake
+++ eng/native/override/configurecompiler.cmake
@@ -5,4 +5,7 @@ if (MSVC)
   add_linker_flag(/INCREMENTAL:NO DEBUG) # prevent "warning LNK4075: ignoring '/INCREMENTAL' due to '/OPT:REF' specification"
   add_linker_flag(/OPT:REF DEBUG)
   add_linker_flag(/OPT:NOICF DEBUG)
+
+  add_compile_options(/sdl)
+  add_compile_options(/Qspectre)
 endif(MSVC)
