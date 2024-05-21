9e2df89e07bb3f887fe872b9fcdf1512d7dc68a0
Joe Schmitt <joschmit@microsoft.com>
2024-05-21 14:23:37 -0700

    Add comment

--- src/Profilers/MonitorProfiler/Communication/MessageCallbackManager.h
+++ src/Profilers/MonitorProfiler/Communication/MessageCallbackManager.h
@@ -23,6 +23,11 @@ class MessageCallbackManager
     private:
         bool TryGetCallback(unsigned short commandSet, std::function<HRESULT (const IpcMessage& message)>& callback);
         std::unordered_map<unsigned short, std::function<HRESULT (const IpcMessage& message)>> m_callbacks;
+        //
+        // Ideally we would use a single std::shared_mutex instead, but we are targeting C++11 without
+        // an easy way to upgrade to C++17 at this time, so we use two separate mutexes instead to
+        // allow for IsRegistered to be used while a DispatchMessage call is in progress.
+        //
         std::mutex m_dispatchMutex;
         std::mutex m_lookupMutex;
 };
