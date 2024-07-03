476dfb621d4f97f11ae9e98d7caca6e118814921
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2024-07-03 09:47:47 -0700

    [Nullable Context] Enable on `Microsoft.Diagnostics.Monitoring.ConfigurationSchema` (#6932)

--- src/Tools/dotnet-monitor/CollectionRules/Options/TemplateOptions.cs
+++ src/Tools/dotnet-monitor/CollectionRules/Options/TemplateOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using Microsoft.Diagnostics.Monitoring.WebApi;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -15,21 +17,21 @@ namespace Microsoft.Diagnostics.Tools.Monitor.CollectionRules.Options
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectionRuleOptions_Filters))]
-        public IDictionary<string, ProcessFilterDescriptor> CollectionRuleFilters { get; set; } = new Dictionary<string, ProcessFilterDescriptor>();
+        public IDictionary<string, ProcessFilterDescriptor>? CollectionRuleFilters { get; set; } = new Dictionary<string, ProcessFilterDescriptor>();
 
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectionRuleOptions_Trigger))]
-        public IDictionary<string, CollectionRuleTriggerOptions> CollectionRuleTriggers { get; set; } = new Dictionary<string, CollectionRuleTriggerOptions>();
+        public IDictionary<string, CollectionRuleTriggerOptions>? CollectionRuleTriggers { get; set; } = new Dictionary<string, CollectionRuleTriggerOptions>();
 
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectionRuleOptions_Actions))]
-        public IDictionary<string, CollectionRuleActionOptions> CollectionRuleActions { get; set; } = new Dictionary<string, CollectionRuleActionOptions>();
+        public IDictionary<string, CollectionRuleActionOptions>? CollectionRuleActions { get; set; } = new Dictionary<string, CollectionRuleActionOptions>();
 
         [Display(
             ResourceType = typeof(OptionsDisplayStrings),
             Description = nameof(OptionsDisplayStrings.DisplayAttributeDescription_CollectionRuleOptions_Limits))]
-        public IDictionary<string, CollectionRuleLimitsOptions> CollectionRuleLimits { get; set; } = new Dictionary<string, CollectionRuleLimitsOptions>();
+        public IDictionary<string, CollectionRuleLimitsOptions>? CollectionRuleLimits { get; set; } = new Dictionary<string, CollectionRuleLimitsOptions>();
     }
 }
