14817b420e9a1c5eb18fff23be7fd086ef290288
kkeirstead <kkeirstead@microsoft.com>
2025-03-10 09:50:27 -0700

    PR Feedback

--- src/Microsoft.Diagnostics.Monitoring.Options/MonitorCapability.cs
+++ src/Microsoft.Diagnostics.Monitoring.Options/MonitorCapability.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.Diagnostics.Monitoring.Options
@@ -8,10 +9,11 @@ namespace Microsoft.Diagnostics.Monitoring.Options
     public class MonitorCapability : IMonitorCapability
     {
         [JsonPropertyName("name")]
-        public string Name { get; init; }
+        [Required]
+        public string Name { get; }
 
         [JsonPropertyName("enabled")]
-        public bool Enabled { get; init; }
+        public bool Enabled { get; }
 
         public MonitorCapability(string name, bool enabled)
         {
