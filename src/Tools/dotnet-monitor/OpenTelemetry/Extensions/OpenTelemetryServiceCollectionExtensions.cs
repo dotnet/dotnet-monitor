5709290da0af2b45d51816c3a3ca1b240c2738fe
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-04-24 09:41:14 -0700

    Add dotnet-monitor files from PoC.

---
+++ src/Tools/dotnet-monitor/OpenTelemetry/Extensions/OpenTelemetryServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Diagnostics.Tools.Monitor.OpenTelemetry;
+
+public static class OpenTelemetryServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureOpenTelemetry(
+        this IServiceCollection services)
+        => ConfigureOpenTelemetry(services, configurationSectionName: "OpenTelemetry");
+
+    public static IServiceCollection ConfigureOpenTelemetry(
+        this IServiceCollection services,
+        string configurationSectionName)
+    {
+        if (string.IsNullOrEmpty(configurationSectionName))
+        {
+            throw new ArgumentNullException(nameof(configurationSectionName));
+        }
+
+        services.AddOptions();
+
+        services.TryAddSingleton<IOptionsFactory<OpenTelemetryOptions>>(sp =>
+        {
+            IConfiguration config = sp.GetRequiredService<IConfiguration>();
+
+            return new OpenTelemetryOptionsFactory(
+                config.GetSection(configurationSectionName),
+                sp.GetServices<IConfigureOptions<OpenTelemetryOptions>>(),
+                sp.GetServices<IPostConfigureOptions<OpenTelemetryOptions>>(),
+                sp.GetServices<IValidateOptions<OpenTelemetryOptions>>());
+        });
+
+        services.AddSingleton<IValidateOptions<OpenTelemetryOptions>, DataAnnotationValidateOptions<OpenTelemetryOptions>>();
+
+        services.AddHostedService<OpenTelemetryService>();
+        services.AddSingleton<OpenTelemetryEndpointManager>();
+        services.AddSingleton<IEndpointInfoSourceCallbacks, OpenTelemetryEndpointInfoSourceCallbacks>();
+
+        return services;
+    }
+}
---
+++ src/Tools/dotnet-monitor/OpenTelemetry/OpenTelemetryEndpointInfoSourceCallbacks.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Monitoring.WebApi;
+
+namespace Microsoft.Diagnostics.Tools.Monitor.OpenTelemetry;
+
+internal sealed class OpenTelemetryEndpointInfoSourceCallbacks : IEndpointInfoSourceCallbacks
+{
+    private readonly OpenTelemetryEndpointManager _openTelemetryManager;
+
+    public OpenTelemetryEndpointInfoSourceCallbacks(OpenTelemetryEndpointManager openTelemetryManager)
+    {
+        _openTelemetryManager = openTelemetryManager;
+    }
+
+    Task IEndpointInfoSourceCallbacks.OnAddedEndpointInfoAsync(IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    Task IEndpointInfoSourceCallbacks.OnBeforeResumeAsync(IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+    {
+        _openTelemetryManager.StartListeningToEndpoint(endpointInfo);
+        return Task.CompletedTask;
+    }
+
+    Task IEndpointInfoSourceCallbacks.OnRemovedEndpointInfoAsync(IEndpointInfo endpointInfo, CancellationToken cancellationToken)
+    {
+        _openTelemetryManager.StopListeningToEndpoint(endpointInfo);
+        return Task.CompletedTask;
+    }
+}
