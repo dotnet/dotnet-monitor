b3d846543c877caf9600962d84f462592f7e9489
Sven Boemer <sbomer@gmail.com>
2025-05-06 21:35:30 +0000

    Remove unused using

--- src/Tools/dotnet-monitor/Egress/FileSystem/FileSystemEgressExtension.cs
+++ src/Tools/dotnet-monitor/Egress/FileSystem/FileSystemEgressExtension.cs
@@ -4,7 +4,6 @@
 using Microsoft.Diagnostics.Tools.Monitor.Egress.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Binder.SourceGeneration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
