6031690b4f642ed77d43707220fdc29635febdca
Wiktor Kopec <wiktork@microsoft.com>
2025-08-27 11:36:45 -0700

    Use dotnet-monitor asset instead of merge manfest for versioning (#8563)

--- eng/release/Scripts/GetBuildVersion.ps1
+++ eng/release/Scripts/GetBuildVersion.ps1
@@ -15,7 +15,7 @@ $buildData = & $PSScriptRoot\GetDarcBuild.ps1 `
     -MaestroApiEndPoint $MaestroApiEndPoint `
     -DarcVersion $DarcVersion
 
-[array]$matchingData = $buildData.assets | Where-Object { $_.name -match 'MergedManifest.xml$' -and $_.nonShipping }
+[array]$matchingData = $buildData.assets | Where-Object { $_.name -eq 'dotnet-monitor' }
 
 if (!$matchingData -or $matchingData.Length -ne 1) {
     Write-Error 'Unable to obtain build version.'
